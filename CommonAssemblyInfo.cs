d13e45b8c7255b74b28a6d4044a194b00be172f6
artem-dudarev <ad@virtoway.com>
2016-06-07 15:16:49 +0200

    Added CommonAssemblyInfo.cs to every project

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="GlobalAssemblyInfo.cs" company="VirtoCommerce">
+//   Copyright © VirtoCommerce. All rights reserved.
+// </copyright>
+// <summary>
+//   Virto Commerce
+// </summary>
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("VirtoCommerce")]
+[assembly: AssemblyProduct("Virto Commerce Catalog Module")]
+[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
+
+[assembly: AssemblyFileVersion("2.11.0.0")]
+[assembly: AssemblyVersion("2.11.0.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
