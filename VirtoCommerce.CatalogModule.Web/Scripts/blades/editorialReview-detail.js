86441d617dcaec43850fb3404b435039b918918d
eMazeika <em@virtoway.com>
2016-10-07 18:39:04 +0300

    HTML editor switched to ui-kit-html-editor (MarkDown)

--- VirtoCommerce.CatalogModule.Web/Scripts/blades/editorialReview-detail.js
+++ VirtoCommerce.CatalogModule.Web/Scripts/blades/editorialReview-detail.js
@@ -1,65 +1,79 @@
 ﻿angular.module('virtoCommerce.catalogModule')
-.controller('virtoCommerce.catalogModule.editorialReviewDetailController', ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.catalogModule.items', 'platformWebApp.settings', function ($scope, bladeNavigationService, items, settings) {
-	var blade = $scope.blade;
-	blade.isLoading = false;
+.controller('virtoCommerce.catalogModule.editorialReviewDetailController', ['$scope', 'platformWebApp.bladeNavigationService', 'FileUploader', 'platformWebApp.settings', '$timeout', function ($scope, bladeNavigationService, FileUploader, settings, $timeout) {
+    var blade = $scope.blade;
 
-	blade.refresh = function (item) {
-		initilize(item);
-	};
+    function initilize() {
+        if (!blade.item.reviews) {
+            blade.item.reviews = [];
+        };
+        if (!blade.currentEntity) {
+            blade.currentEntity = {};
+        };
 
-	function initilize(item) {
-		blade.title = 'catalog.blades.editorialReview-detail.title';
-		blade.subtitle = 'catalog.blades.editorialReview-detail.subtitle';
+        blade.origEntity = blade.currentEntity;
+        blade.currentEntity = angular.copy(blade.currentEntity);
+        if (!blade.currentEntity.languageCode) {
+            blade.currentEntity.languageCode = blade.item.catalog.defaultLanguage.languageCode;
+        }
 
-		if (!blade.item.reviews) {
-			blade.item.reviews = [];
-		};
-		if (!blade.currentEntity) {
-			blade.currentEntity = {};
-		};		
+        $timeout(function () {
+            $scope.$broadcast('resetContent', { body: blade.currentEntity.content });
+            blade.isLoading = false;
+        });
+    }
 
-		blade.origEntity = blade.currentEntity;
-		blade.currentEntity = angular.copy(blade.currentEntity);
-		if(!blade.currentEntity.languageCode)
-		{
-			blade.currentEntity.languageCode = blade.item.catalog.defaultLanguage.languageCode;
-		}
-	};
-
-    settings.getValues({ id: 'Catalog.EditorialReviewTypes' }, function (data) {
-    	$scope.types = data;
-    	if (!blade.currentEntity.reviewType) {
-    		blade.currentEntity.reviewType = $scope.types[0];
-    	}
-    });
-  
     $scope.isValid = true;
 
     $scope.saveChanges = function () {
-    	var existReview = _.find(blade.item.reviews, function (x) { return x == blade.origEntity; });
-    	if (!existReview) {
-    		blade.item.reviews.push(blade.origEntity);
-    	};
-    	angular.copy(blade.currentEntity, blade.origEntity);		
-    	$scope.bladeClose();
-    };   
+        var existReview = _.find(blade.item.reviews, function (x) { return x == blade.origEntity; });
+        if (!existReview) {
+            blade.item.reviews.push(blade.origEntity);
+        };
+        angular.copy(blade.currentEntity, blade.origEntity);
+        $scope.bladeClose();
+    };
 
     blade.headIcon = 'fa-comments';
+    blade.title = 'catalog.blades.editorialReview-detail.title';
+    blade.subtitle = 'catalog.blades.editorialReview-detail.subtitle';
+    blade.editAsMarkdown = true;
+    blade.hasAssetCreatePermission = bladeNavigationService.checkPermission('platform:asset:create');
 
-    blade.toolbarCommands = [];
+    if (blade.hasAssetCreatePermission) {
+        $scope.fileUploader = new FileUploader({
+            url: 'api/platform/assets?folderUrl=catalog/' + blade.item.code,
+            headers: { Accept: 'application/json' },
+            autoUpload: true,
+            removeAfterUpload: true,
+            onBeforeUploadItem: function (fileItem) {
+                blade.isLoading = true;
+            },
+            onSuccessItem: function (fileItem, response) {
+                $scope.$broadcast('filesUploaded', { items: response });
+            },
+            onErrorItem: function (fileItem, response, status) {
+                bladeNavigationService.setError(fileItem._file.name + ' failed: ' + (response.message ? response.message : status), blade);
+            },
+            onCompleteAll: function () {
+                blade.isLoading = false;
+            }
+        });
+    }
+
+    settings.getValues({ id: 'Catalog.EditorialReviewTypes' }, function (data) {
+        $scope.types = data;
+        if (!blade.currentEntity.reviewType) {
+            blade.currentEntity.reviewType = $scope.types[0];
+        }
+    });
 
     $scope.openDictionarySettingManagement = function () {
-        var newBlade = {
-            id: 'settingDetailChild',
-            isApiSave: true,
-            currentEntityId: 'Catalog.EditorialReviewTypes',
-            parentRefresh: function (data) { $scope.types = data; },
-            controller: 'platformWebApp.settingDictionaryController',
-            template: '$(Platform)/Scripts/app/settings/blades/setting-dictionary.tpl.html'
+        var newBlade = new DictionarySettingDetailBlade('Catalog.EditorialReviewTypes');
+        newBlade.parentRefresh = function (data) {
+            $scope.types = data;
         };
         bladeNavigationService.showBlade(newBlade, blade);
     };
 
-    initilize(blade.currentEntity);
-
+    initilize();
 }]);
