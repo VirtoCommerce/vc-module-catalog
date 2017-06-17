076d359befec6c6d1baaa645e73ebe01f8b8ff6b
artem-dudarev <ad@virtoway.com>
2017-06-17 23:59:33 +0200

    Allow to use multiple terms with the same name and join them with AND operator

--- VirtoCommerce.CatalogModule.Data/Search/QueryStringExtensions.cs
+++ VirtoCommerce.CatalogModule.Data/Search/QueryStringExtensions.cs
@@ -19,10 +19,7 @@ namespace VirtoCommerce.CatalogModule.Data.Search
                 result.AddRange(query
                     .Select(item => item.Split(nameValueDelimeter, 2))
                     .Where(item => item.Length == 2)
-                    .Select(item => new StringKeyValues { Key = item[0], Values = item[1].Split(valuesDelimeter, StringSplitOptions.RemoveEmptyEntries) })
-                    .GroupBy(item => item.Key)
-                    .Select(g => new StringKeyValues { Key = g.Key, Values = g.SelectMany(i => i.Values).Distinct().ToArray() })
-                    );
+                    .Select(item => new StringKeyValues { Key = item[0], Values = item[1].Split(valuesDelimeter, StringSplitOptions.RemoveEmptyEntries) }));
             }
 
             return result;
