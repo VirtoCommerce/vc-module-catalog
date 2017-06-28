f89c75b4d6f9019d6f91d1f253cfa91fe54d7cab
Tatarincev <et@virtoway.com>
2017-06-28 12:47:46 +0200

    Little refactoring

--- VirtoCommerce.CatalogModule.Data/Services/CategoryServiceImpl.cs
+++ VirtoCommerce.CatalogModule.Data/Services/CategoryServiceImpl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using CacheManager.Core;
 using Omu.ValueInjecter;
+using VirtoCommerce.CatalogModule.Data.Extensions;
 using VirtoCommerce.CatalogModule.Data.Model;
 using VirtoCommerce.CatalogModule.Data.Repositories;
 using VirtoCommerce.Domain.Catalog.Model;
@@ -194,14 +195,8 @@ namespace VirtoCommerce.CatalogModule.Data.Services
                 CategoryEntity[] entities = null;
                 using (var repository = _repositoryFactory())
                 {
-                    //EF multi-thread issue for cached entities
-                    //http://stackoverflow.com/questions/29106477/nullreferenceexception-in-entity-framework-from-trygetcachedrelatedend
-                    if (repository is System.Data.Entity.DbContext)
-                    {
-                        var dbConfiguration = ((System.Data.Entity.DbContext)repository).Configuration;
-                        dbConfiguration.ProxyCreationEnabled = false;
-                        dbConfiguration.AutoDetectChangesEnabled = false;
-                    }
+                    repository.DisableChangesTracking();
+
                     entities = repository.GetCategoriesByIds(repository.Categories.Select(x => x.Id).ToArray(), Domain.Catalog.Model.CategoryResponseGroup.Full);
                 }
 
