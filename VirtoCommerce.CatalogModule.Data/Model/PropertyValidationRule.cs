3702b83526f44542fdbab7727a5379d2e6fe1672
ksa <ksa@virtoway.com>
2017-07-05 11:15:41 +0200

    propety validation entity object

---
+++ VirtoCommerce.CatalogModule.Data/Model/PropertyValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtoCommerce.CatalogModule.Data.Model
+{
+    public class PropertyValidationRule
+    {
+        public bool IsUnique { get; set; }
+
+        public int? CharCountMin { get; set; }
+
+        public int? CharCountMax { get; set; }
+
+        [StringLength(2048)]
+        public string RegExp { get; set; }
+
+        #region Navigation properties
+
+        public string PropertyId { get; set; }
+
+        public PropertyEntity Property { get; set; }
+
+        #endregion
+    }
+}
