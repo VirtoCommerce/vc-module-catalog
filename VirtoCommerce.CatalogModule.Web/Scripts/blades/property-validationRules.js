f8ed148eaed99c1c9077cf80b2f7be7db5beee36
ksa <ksa@virtoway.com>
2017-07-07 18:57:35 +0200

    saving properties

--- VirtoCommerce.CatalogModule.Web/Scripts/blades/property-validationRules.js
+++ VirtoCommerce.CatalogModule.Web/Scripts/blades/property-validationRules.js
@@ -1,33 +1,60 @@
 ﻿angular.module('virtoCommerce.catalogModule')
     .controller('virtoCommerce.catalogModule.propertyValidationRulesController', ['$scope', function ($scope) {
+        var parentEntity = $scope.blade.parentBlade.currentEntity;
+        var rule = $scope.blade.parentBlade.currentEntity.validationRule;
+        if (!rule) rule = {};
+
         $scope.blade.propertyValidationRule = {
-            isUniquie: false,
-            isLimited: false,
-            isSpecificPattern:false,
-            charCountMin: undefined,
-            charCountMax: undefined,
+            id: rule.id,
+            required: parentEntity.required,
+            isLimited: rule.charCountMin != null || rule.charCountMax != null,
+            isSpecificPattern: rule.regExp != null,
+            charCountMin: rule.charCountMin,
+            charCountMax: rule.charCountMax,
             selectedLimit: 'beetween',
-            selectedPattern:'custom',
+            selectedPattern: 'custom',
             validationPattern: ['custom', 'email', 'url', 'date'],
             characterLimit: ['beetween', 'at least', 'not more than'],
-            seatPattern:'',
-            pattern: ''
+            seatPattern: rule.regExp,
+            pattern: rule.regExp
         };
+
         $scope.checkSelectedPattern = function (selectedPattern) {
-            if (selectedPattern == 'email')
-                $scope.blade.propertyValidationRule.seatPattern = '/^([\w\._]+)@\1\.([a-z]{2,6}\.?)$/';
-            else if (selectedPattern == 'url')
+            if (selectedPattern === 'email')
+                $scope.blade.propertyValidationRule.seatPattern = "/\S+@\S+\.\S+/";
+            else if (selectedPattern === 'url')
                 $scope.blade.propertyValidationRule.seatPattern = '/^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/';
-            else if (selectedPattern == 'date')
+            else if (selectedPattern === 'date')
                 $scope.blade.propertyValidationRule.seatPattern = '(0[1-9]|[12][0-9]|3[01])[ \.-](0[1-9]|1[012])[ \.-](19|20|)\d\d';
-            else if 
-                return;
         }
-    
-    $scope.selectOption = function (option) {
-    	$scope.blade.property.valueType = option;
-        $scope.bladeClose();
+
+        $scope.selectOption = function (option) {
+            $scope.blade.property.valueType = option;
+            $scope.bladeClose();
         };
-    $scope.blade.headIcon = 'fa-gear';
-    $scope.blade.isLoading = false;
-}]);
+
+        $scope.blade.headIcon = 'fa-gear';
+        $scope.blade.isLoading = false;
+
+
+        $scope.saveChanges = function () {
+            $scope.blade.parentBlade.currentEntity.required = $scope.blade.propertyValidationRule.required;
+            $scope.blade.parentBlade.currentEntity.validationRule = {
+                id: $scope.blade.propertyValidationRule.id,
+                charCountMin: $scope.blade.propertyValidationRule.charCountMin,
+                charCountMax: $scope.blade.propertyValidationRule.charCountMax,
+                regExp: $scope.blade.propertyValidationRule.seatPattern
+            };
+            $scope.bladeClose();
+        };
+
+        var formScope;
+        $scope.setForm = function (form) {
+            formScope = form;
+        }
+
+        $scope.$watch('blade.propertyValidationRule', function () {
+            $scope.isValid = formScope && formScope.$valid;
+        }, true);
+
+    }]);
