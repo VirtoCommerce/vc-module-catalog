VirtoCommerce/vc-module-catalog

commit 92cab11d126e0795e19b1cac636f0cf39edfd5c1

Minor bug fixes and optimizations

VirtoCommerce.CatalogModule.Data/Services/ItemServiceImpl.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using CacheManager.Core;
-using VirtoCommerce.CatalogModule.Data.Converters;
 using VirtoCommerce.CatalogModule.Data.Model;
 using VirtoCommerce.CatalogModule.Data.Repositories;
 using VirtoCommerce.Domain.Catalog.Model;
@@ -11,7 +9,6 @@ using VirtoCommerce.Domain.Catalog.Services;
 using VirtoCommerce.Domain.Commerce.Model;
 using VirtoCommerce.Domain.Commerce.Services;
 using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.Platform.Data.Common;
 using VirtoCommerce.Platform.Data.Infrastructure;
 
 
@@ -53,8 +50,7 @@ namespace VirtoCommerce.CatalogModule.Data.Services
             }
 
             LoadProductDependencies(result);
-           
-            
+            ApplyInheritanceRules(result);
 
             // Fill outlines for products
             if (respGroup.HasFlag(ItemResponseGroup.Outlines))
@@ -78,12 +74,40 @@ namespace VirtoCommerce.CatalogModule.Data.Services
                 _commerceService.LoadSeoForObjects(objectsWithSeo.ToArray());
             }
 
-            //Cleanup result model considered requested response group
+            //Reduce details according to response group
             foreach (var product in result)
             {
+                if (!respGroup.HasFlag(ItemResponseGroup.ItemAssets))
+                {
+                    product.Assets = null;
+                }
+                if (!respGroup.HasFlag(ItemResponseGroup.ItemAssociations))
+                {
+                    product.Associations = null;
+                }
+                if (!respGroup.HasFlag(ItemResponseGroup.ItemEditorialReviews))
+                {
+                    product.Reviews = null;
+                }
                 if (!respGroup.HasFlag(ItemResponseGroup.ItemProperties))
                 {
                     product.Properties = null;
+                }
+                if (!respGroup.HasFlag(ItemResponseGroup.Links))
+                {
+                    product.Links = null;
+                }
+                if (!respGroup.HasFlag(ItemResponseGroup.Outlines))
+                {
+                    product.Outlines = null;
+                }
+                if (!respGroup.HasFlag(ItemResponseGroup.Seo))
+                {
+                    product.SeoInfos = null;
+                }
+                if (!respGroup.HasFlag(ItemResponseGroup.Variations))
+                {
+                    product.Variations = null;
                 }
             }
 
@@ -152,31 +176,144 @@ namespace VirtoCommerce.CatalogModule.Data.Services
         }
 
         
-        protected virtual void LoadProductDependencies(CatalogProduct[] products)
+        protected virtual void LoadProductDependencies(CatalogProduct[] products, bool processVariations = true)
+        {
+            var catalogsMap = _catalogService.GetCatalogsList().ToDictionary(x => x.Id);
+            var allCategoriesIds = products.Select(x => x.CategoryId).Distinct().ToArray();
+            var categoriesMap = _categoryService.GetByIds(allCategoriesIds, CategoryResponseGroup.Full).ToDictionary(x => x.Id);
+
+            foreach (var product in products)
+            {
+                product.Catalog = catalogsMap[product.CatalogId];
+                if (product.CategoryId != null)
+                {
+                    product.Category = categoriesMap[product.CategoryId];
+                }
+
+                if (product.Links != null)
+                {
+                    foreach (var link in product.Links)
+                    {
+                        link.Catalog = catalogsMap[link.CatalogId];
+                        link.Category = _categoryService.GetById(link.CategoryId, CategoryResponseGroup.WithProperties);
+                    }
+                }
+
+                if (product.MainProduct != null)
+                {
+                    LoadProductDependencies(new[] { product.MainProduct }, false);
+                }
+                if (processVariations && !product.Variations.IsNullOrEmpty())
+                {
+                    LoadProductDependencies(product.Variations.ToArray());
+                }
+            }
+        }
+
+        protected virtual void ApplyInheritanceRules(CatalogProduct[] products)
         {
             foreach (var product in products)
             {
-                product.Catalog = _catalogService.GetById(product.CatalogId);
-                if (product.CategoryId != null)
+                //Inherit images from parent product (if its not set)
+                if (!product.Images.Any() && product.MainProduct != null && !product.MainProduct.Images.IsNullOrEmpty())
                 {
-                    product.Category = _categoryService.GetById(product.CategoryId, CategoryResponseGroup.Info);
-                }
-
-                foreach (var link in product.Links)
-                {
-                    link.Catalog = _catalogService.GetById(link.CatalogId);
-                    link.Category = _categoryService.GetById(link.CategoryId, CategoryResponseGroup.Info);
-                }
-
-                foreach (var property in product.Properties)
-                {
-                    property.Catalog = _catalogService.GetById(property.CatalogId);
-                    if (property.CategoryId != null)
+                    product.Images = product.MainProduct.Images.Select(x => x.Clone()).OfType<Image>().ToList();
+                    foreach (var image in product.Images)
                     {
-                        property.Category = _categoryService.GetById(property.CategoryId, CategoryResponseGroup.Info);
+                        image.Id = null;
+                        image.IsInherited = true;
                     }
                 }
-                LoadProductDependencies(product.Variations.ToArray());              
+
+                //Inherit assets from parent product (if its not set)
+                if (!product.Assets.Any() && product.MainProduct != null && product.MainProduct.Assets != null)
+                {
+                    product.Assets = product.MainProduct.Assets.Select(x => x.Clone()).OfType<Asset>().ToList();
+                    foreach (var asset in product.Assets)
+                    {
+                        asset.Id = null;
+                        asset.IsInherited = true;
+                    }
+                }
+
+                //inherit editorial reviews from main product and do not inherit if variation loaded within product
+                if (!product.Reviews.Any() && product.MainProduct != null && product.MainProduct.Reviews != null)
+                {
+                    product.Reviews = product.MainProduct.Reviews.Select(x => x.Clone()).OfType<EditorialReview>().ToList();
+                    foreach (var review in product.Reviews)
+                    {
+                        review.Id = null;
+                        review.IsInherited = true;
+                    }
+                }
+
+                //TaxType category inheritance
+                if (product.TaxType == null && product.Category != null)
+                {
+                    product.TaxType = product.Category.TaxType;
+                }
+
+                //Properties inheritance
+                product.Properties = (product.Category != null ? product.Category.Properties : product.Catalog.Properties).Select(x => x.Clone())
+                                     .OfType<Property>()
+                                     .OrderBy(x => x.Name)
+                                     .ToList();
+                foreach (var property in product.Properties)
+                {
+                    property.IsInherited = true;
+                }
+
+                //Self item property values
+                foreach (var propertyValue in product.PropertyValues.ToArray())
+                {
+                    //Try to find property meta information
+                    propertyValue.Property = product.Properties.FirstOrDefault(x => x.IsSuitableForValue(propertyValue));
+                    //Return each localized value for selected dictionary value
+                    //Because multilingual dictionary values for all languages may not stored in db need add it in result manually from property dictionary values
+                    var localizedDictValues = propertyValue.TryGetAllLocalizedDictValues();
+                    foreach (var localizedDictValue in localizedDictValues)
+                    {
+                        if (!product.PropertyValues.Any(x => x.ValueId == localizedDictValue.ValueId && x.LanguageCode == localizedDictValue.LanguageCode))
+                        {
+                            product.PropertyValues.Add(localizedDictValue);
+                        }
+                    }
+                }
+
+                //inherit not overriden property values from main product
+                if (product.MainProduct != null && product.MainProduct.PropertyValues != null)
+                {
+                    var mainProductPopValuesGroups = product.MainProduct.PropertyValues.GroupBy(x => x.PropertyName);
+                    foreach (var group in mainProductPopValuesGroups)
+                    {
+                        //Inherit all values if not overriden
+                        if (!product.PropertyValues.Any(x => x.PropertyName.EqualsInvariant(group.Key)))
+                        {
+                            foreach (var inheritedpropValue in group)
+                            {
+                                inheritedpropValue.Id = null;
+                                inheritedpropValue.IsInherited = true;
+                                product.PropertyValues.Add(inheritedpropValue);
+                            }
+                        }
+                    }
+                }
+                //Measurement inheritance 
+                if(product.MainProduct != null)
+                {
+                    product.Width = product.Width ?? product.MainProduct.Width;
+                    product.Height = product.Height ?? product.MainProduct.Height;
+                    product.Length = product.Length ?? product.MainProduct.Length;
+                    product.MeasureUnit = product.MeasureUnit ?? product.MainProduct.MeasureUnit;
+                    product.Weight = product.Weight ?? product.MainProduct.Weight;
+                    product.WeightUnit = product.WeightUnit ?? product.MainProduct.WeightUnit;
+                    product.PackageType = product.PackageType ?? product.MainProduct.PackageType;
+                }
+
+                if (!product.Variations.IsNullOrEmpty())
+                {
+                    ApplyInheritanceRules(product.Variations.ToArray());
+                }
             }
         }
     }

CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Catalog Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
 
-[assembly: AssemblyFileVersion("2.16.7.0")]
-[assembly: AssemblyVersion("2.16.7.0")]
+[assembly: AssemblyFileVersion("2.17.1.0")]
+[assembly: AssemblyVersion("2.17.1.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

VirtoCommerce.CatalogModule.Data/Migrations/201706261011171_PropertyDisplayNames.Designer.cs
@@ -1,0 +1,29 @@
+// <auto-generated />
+namespace VirtoCommerce.CatalogModule.Data.Migrations
+{
+    using System.CodeDom.Compiler;
+    using System.Data.Entity.Migrations;
+    using System.Data.Entity.Migrations.Infrastructure;
+    using System.Resources;
+    
+    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
+    public sealed partial class PropertyDisplayNames : IMigrationMetadata
+    {
+        private readonly ResourceManager Resources = new ResourceManager(typeof(PropertyDisplayNames));
+        
+        string IMigrationMetadata.Id
+        {
+            get { return "201706261011171_PropertyDisplayNames"; }
+        }
+        
+        string IMigrationMetadata.Source
+        {
+            get { return null; }
+        }
+        
+        string IMigrationMetadata.Target
+        {
+            get { return Resources.GetString("Target"); }
+        }
+    }
+}

VirtoCommerce.CatalogModule.Data/Migrations/201706261011171_PropertyDisplayNames.cs
@@ -1,0 +1,34 @@
+namespace VirtoCommerce.CatalogModule.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class PropertyDisplayNames : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PropertyDisplayName",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Locale = c.String(maxLength: 64),
+                    Name = c.String(maxLength: 512),
+                    PropertyId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.PropertyId);
+
+            Sql(@"INSERT INTO dbo.PropertyDisplayName ([Id], [Locale], [Name], [PropertyId])
+                  SELECT [Id], RIGHT(PropertyAttributeName, LEN(PropertyAttributeName) - LEN('DisplayName')) as [Locale], PropertyAttributeValue as [Name], [PropertyId] FROM dbo.PropertyAttribute
+                  WHERE [PropertyAttributeName] LIKE 'DisplayName%'");
+
+        }
+
+        public override void Down()
+        {
+            DropIndex("dbo.PropertyDisplayName", new[] { "PropertyId" });
+            DropTable("dbo.PropertyDisplayName");
+        }
+    }
+}

commit 0db5d38f21769d32b5b293e35d9139bcce178452

Fixed empty parents for category in product link (broken since 92cab11d)

VirtoCommerce.CatalogModule.Data/Services/ItemServiceImpl.cs
@@ -6,14 +6,13 @@ using FluentValidation;
 using VirtoCommerce.CatalogModule.Data.Extensions;
 using VirtoCommerce.CatalogModule.Data.Model;
 using VirtoCommerce.CatalogModule.Data.Repositories;
+using VirtoCommerce.CatalogModule.Data.Services.Validation;
 using VirtoCommerce.Domain.Catalog.Model;
 using VirtoCommerce.Domain.Catalog.Services;
 using VirtoCommerce.Domain.Commerce.Model;
 using VirtoCommerce.Domain.Commerce.Services;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Data.Infrastructure;
-using VirtoCommerce.CatalogModule.Data.Services.Validation;
-using VirtoCommerce.Platform.Core.Exceptions;
 
 namespace VirtoCommerce.CatalogModule.Data.Services
 {
@@ -207,7 +206,7 @@ namespace VirtoCommerce.CatalogModule.Data.Services
             {
                 product.Catalog = catalogsMap.GetValueOrThrow(product.CatalogId, $"catalog with key {product.CatalogId} not exist");
                 if (product.CategoryId != null)
-                {             
+                {
                     product.Category = categoriesMap.GetValueOrThrow(product.CategoryId, $"category with key {product.CategoryId} not exist");
                 }
 
@@ -216,7 +215,7 @@ namespace VirtoCommerce.CatalogModule.Data.Services
                     foreach (var link in product.Links)
                     {
                         link.Catalog = catalogsMap.GetValueOrThrow(link.CatalogId, $"link catalog with key {link.CatalogId} not exist");
-                        link.Category = _categoryService.GetById(link.CategoryId, CategoryResponseGroup.WithProperties);
+                        link.Category = _categoryService.GetById(link.CategoryId, CategoryResponseGroup.WithProperties | CategoryResponseGroup.WithParents);
                     }
                 }
 