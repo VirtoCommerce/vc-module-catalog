6e94482587fafbdbe5736128269b442e51f40ae1
ksa <ksa@virtoway.com>
2017-08-08 19:08:25 +0200

    added Code  to product validation errors

--- VirtoCommerce.CatalogModule.Data/Services/Validation/ProductValidator.cs
+++ VirtoCommerce.CatalogModule.Data/Services/Validation/ProductValidator.cs
@@ -13,9 +13,9 @@ namespace VirtoCommerce.CatalogModule.Data.Services.Validation
         private static char[] _illegalCodeChars = new[] { '$', '+', ';', '=', '%', '{', '}', '[', ']', '|', '\\', '/', '@', '~', '!', '^', '*', '&', '(', ')', ':', '<', '>' };
         public ProductValidator()
         {
-            
+
             RuleFor(product => product.CatalogId).NotNull().NotEmpty();
-            RuleFor(product => product.Name).NotNull().NotEmpty().MaximumLength(1024);
+            RuleFor(product => product.Name).NotNull().WithMessage(x => $"Name is null. Code: {x.Code}").NotEmpty().WithMessage(x => $"Name is empty. Code: {x.Code}").MaximumLength(1024);
             RuleFor(product => product.Code).NotNull().NotEmpty().MaximumLength(64).DependentRules(d => d.RuleFor(product => product.Code).Must(x => x.IndexOfAny(_illegalCodeChars) < 0).WithMessage("product code contains illegal chars"));
         }
     }
