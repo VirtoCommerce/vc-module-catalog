f954467393c95231711d4505dd387e23478c633e
Eugeny Tatarincev <et@virtoway.com>
2017-10-23 16:06:22 +0200

    Remove csv import/export from module https://github.com/VirtoCommerce/vc-module-catalog/issues/59

--- VirtoCommerce.CatalogModule.Web.Core/ExportImport/CsvImportInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VirtoCommerce.CatalogModule.Web.ExportImport
-{
-    public class CsvImportInfo
-    {
-        public string CatalogId { get; set; }
-        public string FileUrl { get; set; }
-        public CsvProductMappingConfiguration Configuration { get; set; }
-    }
-}
