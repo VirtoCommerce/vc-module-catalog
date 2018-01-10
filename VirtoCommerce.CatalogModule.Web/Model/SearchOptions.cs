a24a620ed0c2d5e0c1786cfdc95e31007f6dae72
Andrei Iushkevich <Andrei.Iushkevich@outlook.com>
2018-01-10 11:58:00 +0300

    Add SearchOptions class

---
+++ VirtoCommerce.CatalogModule.Web/Model/SearchOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtoCommerce.CatalogModule.Web.Model
+{
+    public class SearchOptions
+    {
+        public int? Count { get; set; }
+        public string Sort { get; set; }
+        public int? Start { get; set; }
+    }
+}
