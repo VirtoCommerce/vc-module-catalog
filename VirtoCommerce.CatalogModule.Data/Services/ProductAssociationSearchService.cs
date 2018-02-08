af8b9acf5558a0495c6799aae9f09bd534922c07
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-02-08 12:04:06 +0200

    fixed errors in ProductAssociationSearchService

--- VirtoCommerce.CatalogModule.Data/Services/ProductAssociationSearchService.cs
+++ VirtoCommerce.CatalogModule.Data/Services/ProductAssociationSearchService.cs
@@ -17,17 +17,11 @@ namespace VirtoCommerce.CatalogModule.Data.Services
   {
     private readonly Func<ICatalogRepository> _catalogRepositoryFactory;
     private readonly IItemService _itemService;
-    private readonly ICategoryService _categoryService;
 
-    private readonly Dictionary<string, string> _productSortingAliases = new Dictionary<string, string>();
-
-    public ProductAssociationSearchService(Func<ICatalogRepository> catalogRepositoryFactory, IItemService itemService, ICategoryService categoryService)
+    public ProductAssociationSearchService(Func<ICatalogRepository> catalogRepositoryFactory, IItemService itemService)
     {
       _catalogRepositoryFactory = catalogRepositoryFactory;
       _itemService = itemService;
-      _categoryService = categoryService;
-
-      _productSortingAliases["sku"] = ReflectionUtility.GetPropertyName<CatalogProduct>(x => x.Code);
     }
 
     public GenericSearchResult<CatalogProduct> SearchProductAssociations(ProductAssociationSearchCriteria criteria)
@@ -39,42 +33,28 @@ namespace VirtoCommerce.CatalogModule.Data.Services
       {
         sortInfos = new[] { new SortInfo { SortColumn = "Priority", SortDirection = SortDirection.Descending }, new SortInfo { SortColumn = "Name", SortDirection = SortDirection.Ascending } };
       }
-      //Try to replace sorting columns names
-      TryTransformSortingInfoColumnNames(_productSortingAliases, sortInfos);
    
-      var productResponseGroup = ItemResponseGroup.ItemInfo | ItemResponseGroup.ItemAssets | ItemResponseGroup.Links | ItemResponseGroup.Seo;
+      var productResponseGroup = ItemResponseGroup.ItemAssociations;
    
       var objectTypes = criteria.ObjectTypes.ToList();
       var objectIds = criteria.ObjectIds;
       var searchCategoryIds = new string[] { };
-      var categoryIds = new string[] { };//new List<string>();
+      var categoryIds = new string[] { };
    
-      var products = _itemService.GetByIds(objectIds.ToArray(), productResponseGroup)
-                              .OrderBy(x => objectIds.IndexOf(x.Id)).ToList();   
+      var products = _itemService.GetByIds(objectIds.ToArray(), productResponseGroup).ToList();   
     
       using (var repository = _catalogRepositoryFactory())
       {
         //Optimize performance and CPU usage
         repository.DisableChangesTracking();
-       
-        foreach (var product in products)
+
+        //Get all products category associations.
+        var categoryAssociations = products.SelectMany(x => x.Associations.Where(a => a.AssociatedObjectType == "category")).ToArray();
+        for( var i = 0; i < categoryAssociations.Length; i++)
         {
-          if (product.Associations.Count() > 0)
-          {
-             for(var i = 0; i < product.Associations.ToArray().Length; i++)
-             {
-               categoryIds[i] = product.CategoryId;
-             }
-          }
-          //if (product.ReferencedAssociations.Count() > 0)
-          //{
-          //  for(var i = 0; i < product.ReferencedAssociations.ToArray().Length; i++)
-          //  {
-          //    categoryIds[i] = product.CategoryId;
-          //  }
-          //}
+          categoryIds[i] = categoryAssociations[i].AssociatedObjectId;
         }
-       
+
         var query = repository.Items.Where(x => x.IsBuyable || x.IsActive);        
      
         if (!categoryIds.IsNullOrEmpty())
@@ -109,23 +89,9 @@ namespace VirtoCommerce.CatalogModule.Data.Services
      
         retVal.TotalCount = query.Count();
         retVal.Results = _itemService.GetByIds(itemIds.ToArray(), productResponseGroup)
-                                        .OrderBy(x => itemIds.IndexOf(x.Id)).ToList();
-     
+                                        .OrderBy(x => itemIds.IndexOf(x.Id)).ToList();     
       }
-    return retVal;
-  }
-
-    protected virtual void TryTransformSortingInfoColumnNames(IDictionary<string, string> transformationMap, SortInfo[] sortingInfos)
-    {
-      //Try to replace sorting columns names
-      foreach (var sortInfo in sortingInfos)
-      {
-        string newColumnName;
-        if (transformationMap.TryGetValue(sortInfo.SortColumn.ToLowerInvariant(), out newColumnName))
-        {
-          sortInfo.SortColumn = newColumnName;
-        }
-      }
+      return retVal;
     }
   }
 }
