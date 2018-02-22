a398135bfa2e9d9d56c7221d9c5e4b6c097acc0c
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-02-22 10:16:30 +0200

    working on tests

--- VirtoCommerce.CatalogModule.Test/ProductAssociationSearchTest.cs
+++ VirtoCommerce.CatalogModule.Test/ProductAssociationSearchTest.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -8,7 +9,9 @@ using VirtoCommerce.CatalogModule.Data.Services;
 using VirtoCommerce.Domain.Catalog.Model;
 using VirtoCommerce.Domain.Catalog.Model.Search;
 using VirtoCommerce.Domain.Catalog.Services;
+using VirtoCommerce.Domain.Commerce.Model.Search;
 using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Data.Infrastructure.Interceptors;
 using Xunit;
 
 namespace VirtoCommerce.CatalogModule.Test
@@ -24,25 +27,96 @@ namespace VirtoCommerce.CatalogModule.Test
       _productAssociationSearchService = new ProductAssociationSearchService(() => catalogRepository.Object, itemService.Object);
     }
 
+    [Fact]
+    public void GetCriteria_InvalidArguments_ThrowsArgumentNullException()
+    {
+      var criteria = GetTestProductAssociationCriteria_Empty();
+      Assert.Throws<ArgumentNullException>(() => _productAssociationSearchService.SearchProductAssociations(criteria));
+    }
+
+    [Fact]
+    public void GetAssociations_ByCriteriaObjectIds()
+    {
+      var criteria = GetTestProductAssociationCriteria();
+      var query = GetTestAssociationEntities().Where(x => criteria.ObjectIds.Contains(x.ItemId)).ToList();
+
+      var associations = new List<AssociationEntity> {
+        new AssociationEntity
+        {
+            ItemId = "source1",
+            AssociatedItemId = "target1",
+            AssociationType = "Related Items",
+        },
+        new AssociationEntity
+        {
+            ItemId = "source1",
+            AssociatedItemId = "target2",
+            AssociationType = "Related Items",
+            Quantity = 1
+        },
+        new AssociationEntity
+        {
+            ItemId = "source1",
+            AssociatedCategoryId = "target3",
+            AssociationType = "category"
+        },
+        new AssociationEntity
+        {
+            ItemId = "source1",
+            AssociatedCategoryId = "target4",
+            AssociationType = "category"
+        }
+      };
+
+      Assert.Equal(associations.Count, query.Count);
+
+      //foreach (var item in query)
+      //  Assert.True(associations.Contains(item));
+      //Assert.True(!associations.Except(query).Any());
+      //Assert.True(associations.OrderBy(i => i).SequenceEqual(query.OrderBy(i => i)));
+    }
+
     //[Fact]
     //public void SearchProductAssociations_Test()
     //{
     //  var criteria = GetTestProductAssociationCriteria();
     //  var retVal = _productAssociationSearchService.SearchProductAssociations(criteria);
 
-    //  var products = GetTestProductEntities();
-    //  var catalogProducts = GetTestData().OfType<CatalogProduct>();
+    //  var catalogRepository = new Mock<ICatalogRepository>();
+    //  var itemService = new Mock<IItemService>();
+      
+    //var products = GetTestProductEntities();
+    //catalogRepository.Setup(r => r.Items).Returns(products);
+    //catalogRepository.Setup(r => r.GetItemByIds(It.IsAny<string[]>(), It.IsAny<ItemResponseGroup>())).Returns((string[] ids, ItemResponseGroup responseGroup) =>
+    //  {
+    //  return products.Where(p => ids.Contains(p.Id)).ToArray();
+    //});
+    //  catalogRepository.Setup(x => x.Associations).Returns(associations.AsQueryable().Where(x => criteria.ObjectIds.Contains(x.ItemId)));
 
-    //  Assert(expectedResult, retVal.Results);
+    //  var expectedResult = catalogProducts.ToList();
+
+    //  Assert.Equal(expectedResult, retVal.Results);
     //}
+
+    private ProductAssociationSearchCriteria GetTestProductAssociationCriteria_Empty()
+    {
+      var criteria = new ProductAssociationSearchCriteria() {
+        ObjectIds = new string[] {  }.ToList(), 
+        ResponseGroup = "ItemInfo",
+        Skip = 1,
+        Take = 1
+      };
+      return criteria;
+    }
 
     private ProductAssociationSearchCriteria GetTestProductAssociationCriteria()
     {
-      var criteria = new ProductAssociationSearchCriteria() {
-        ObjectIds = new string[] {  }.ToList(), //"source1", "source2", "target1", "target2"
-        ResponseGroup = "None",
-        Skip = 2,
-        Take = 2
+      var criteria = new ProductAssociationSearchCriteria()
+      {
+        ObjectIds = new string[] { "source1" }.ToList(),
+        ResponseGroup = "ItemInfo",
+        Skip = 1,
+        Take = 1
       };
       return criteria;
     }
@@ -119,9 +193,9 @@ namespace VirtoCommerce.CatalogModule.Test
       var c3 = new Category { CatalogId = c.Id, Catalog = c, Id = "c3", ParentId = c2.Id, Parents = new[] { c1, c2 } };
       var v1 = new Category { CatalogId = v.Id, Catalog = v, Id = "v1", IsVirtual = true };
       var v2 = new Category { CatalogId = v.Id, Catalog = v, Id = "v2", IsVirtual = true, ParentId = v1.Id, Parents = new[] { v1 } };
-      var p01 = new CatalogProduct { CatalogId = c.Id, Id = "p0", Catalog = c };
-      var p02 = new CatalogProduct { CatalogId = c.Id, Id = "p0", Catalog = c };
-      var p03 = new CatalogProduct { CatalogId = c.Id, Id = "p0", Catalog = c, Associations = new List<ProductAssociation>
+      var p01 = new CatalogProduct { CatalogId = c.Id, Id = "p01", Catalog = c };
+      var p02 = new CatalogProduct { CatalogId = c.Id, Id = "p02", Catalog = c };
+      var p03 = new CatalogProduct { CatalogId = c.Id, Id = "p03", Catalog = c, Associations = new List<ProductAssociation>
       { new ProductAssociation {AssociatedObjectId = "c3", AssociatedObjectType = "category"},
         new ProductAssociation {AssociatedObjectId = "p11", AssociatedObjectType = "product"} }
       };
@@ -139,6 +213,46 @@ namespace VirtoCommerce.CatalogModule.Test
                        };
 
       return new IHasOutlines[] { c0, c1, c2, c3, v1, v2, p01, p02, p03, p11, p12, p13 };
+    }
+
+    private IQueryable<AssociationEntity> GetTestAssociationEntities()
+    {
+      var associations = new List<AssociationEntity>
+            {
+                new AssociationEntity
+                {
+                    ItemId = "source1",
+                    AssociatedItemId = "target1",
+                    AssociationType = "Related Items",
+                },
+                new AssociationEntity
+                {
+                    ItemId = "source1",
+                    AssociatedItemId = "target2",
+                    AssociationType = "Related Items",
+                    Quantity = 1
+                },
+                new AssociationEntity
+                {
+                    ItemId = "source2",
+                    AssociatedItemId = "target1",
+                    AssociationType = "Related Items",
+                    Tags = "test1;test2"
+                },
+                new AssociationEntity
+                {
+                    ItemId = "source1",
+                    AssociatedCategoryId = "target3",
+                    AssociationType = "category"
+                },
+                new AssociationEntity
+                {
+                    ItemId = "source1",
+                    AssociatedCategoryId = "target4",
+                    AssociationType = "category"
+                }
+            };
+      return associations.AsQueryable();
     }
   }
 }
