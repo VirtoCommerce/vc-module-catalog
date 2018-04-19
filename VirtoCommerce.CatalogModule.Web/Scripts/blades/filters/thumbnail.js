00247a93fd1a3788564cb981cddff75d5f052fb3
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-04-19 22:57:19 +0300

    New variant of thumbnail filter

--- VirtoCommerce.CatalogModule.Web/Scripts/blades/filters/thumbnail.js
+++ VirtoCommerce.CatalogModule.Web/Scripts/blades/filters/thumbnail.js
@@ -2,7 +2,7 @@ angular.module('virtoCommerce.catalogModule')
     .filter('thumbnail', function () {
         return function (imageUrl, prefix) {
             var parts = imageUrl.split(".");
-            var thumbnailUrl = parts[0].indexOf('_') + 1 != 0 ? imageUrl : parts[0] + prefix + parts[1];
+            var thumbnailUrl = parts[0].indexOf('_') + 1 != 0 || parts[0].indexOf('-') + 1 != 0 || parts[0].indexOf('.') + 1 != 0 ? imageUrl : parts[0] + prefix + parts[1];
             return thumbnailUrl;
         };
     });
