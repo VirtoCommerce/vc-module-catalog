681157f091415d932e1613116cfb2dfde596a8cd
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-05-28 16:10:49 +0200

    Added empty tests file for CUD operations on property and events publishing

---
+++ VirtoCommerce.CatalogModule.Test/TestEventsPublishing/PropertyEventsPublishTest.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace VirtoCommerce.CatalogModule.Test.TestEventsPublishing
+{
+    public class PropertyEventsPublishTest : BaseEventTest
+    {
+
+        [Fact]
+        public void TestCreatePropertyEvent()
+        {
+
+        }
+
+        [Fact]
+        public void TestUpdatePropertyEvent()
+        {
+
+        }
+
+        [Fact]
+        public void TestDeletePropertyEvent()
+        {
+
+        }
+    }
+}
