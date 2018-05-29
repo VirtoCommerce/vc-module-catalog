1d390ae9951024b03de67e9a092c7b5b2ec73e03
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-05-29 12:13:14 +0200

    Added tests for event publishing on adding or modified Product entry

--- VirtoCommerce.CatalogModule.Test/TestEventsPublishing/ProductEventPublishTest.cs
+++ VirtoCommerce.CatalogModule.Test/TestEventsPublishing/ProductEventPublishTest.cs
@@ -1,12 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
 using FluentValidation;
 using Moq;
+using VirtoCommerce.CatalogModule.Data.Model;
 using VirtoCommerce.CatalogModule.Data.Repositories;
 using VirtoCommerce.CatalogModule.Data.Services;
+using VirtoCommerce.CatalogModule.Web.Model;
+using VirtoCommerce.Domain.Catalog.Events;
 using VirtoCommerce.Domain.Catalog.Model;
 using VirtoCommerce.Domain.Catalog.Services;
 using VirtoCommerce.Domain.Commerce.Services;
+using VirtoCommerce.Domain.Common.Events;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 using Xunit;
+using Catalog = VirtoCommerce.Domain.Catalog.Model.Catalog;
+using Category = VirtoCommerce.Domain.Catalog.Model.Category;
+using Property = VirtoCommerce.Domain.Catalog.Model.Property;
 
 namespace VirtoCommerce.CatalogModule.Test.TestEventsPublishing
 {
@@ -25,21 +35,68 @@ namespace VirtoCommerce.CatalogModule.Test.TestEventsPublishing
             var outlineService = GetMockedOutlineService();
 
             var catalogService = GetMockedCatalogService();
+            catalogService.Setup(c => c.GetCatalogsList()).Returns(new[] {GetCatalog()});
 
             var categoryService = GetMockedCategoryService();
 
             var eventPublisher = GetMockedEventPublisher();
+            var changingEventChangedEntries = new List<GenericChangedEntry<CatalogProduct>>();
+            AssignChangedEntriesToLicalVariable<ProductChangingEvent, CatalogProduct>(
+                eventPublisher,
+                (changedEntry, token) => { changingEventChangedEntries = changedEntry.ChangedEntries.ToList(); });
+
+            var changedEventChangedEntries = new List<GenericChangedEntry<CatalogProduct>>();
+            AssignChangedEntriesToLicalVariable<ProductChangedEvent, CatalogProduct>(
+                eventPublisher,
+                (changedEntry, token) => { changedEventChangedEntries = changedEntry.ChangedEntries.ToList(); });
+
 
             var itemService = GetItemService(catalogRepo.Object, commerceService.Object, outlineService.Object,
                 catalogService.Object, categoryService.Object, GetValidator(), eventPublisher.Object);
 
             itemService.Create(catalogProduct);
+
+            AssertValues<ProductChangingEvent, CatalogProduct>(eventPublisher, changingEventChangedEntries, EntryState.Added);
+            AssertValues<ProductChangedEvent, CatalogProduct>(eventPublisher, changedEventChangedEntries, EntryState.Added);
         }
 
         [Fact]
         public void TestUpdateCatalogEntityEvent()
         {
+            var catalogProduct = GetCatalogProduct();
 
+            var catalogRepo = GetMockedCatalogRepository();
+            catalogRepo
+                .Setup(c => c.GetItemByIds(It.IsAny<string[]>(), It.IsAny<ItemResponseGroup>()))
+                .Returns(new[] {GetItemEntity()});
+
+            var commerceService = GetMockedCommerceService();
+
+            var outlineService = GetMockedOutlineService();
+
+            var catalogService = GetMockedCatalogService();
+            catalogService.Setup(c => c.GetCatalogsList()).Returns(new[] { GetCatalog() });
+
+            var categoryService = GetMockedCategoryService();
+
+            var eventPublisher = GetMockedEventPublisher();
+            var changingEventChangedEntries = new List<GenericChangedEntry<CatalogProduct>>();
+            AssignChangedEntriesToLicalVariable<ProductChangingEvent, CatalogProduct>(
+                eventPublisher,
+                (changedEntry, token) => { changingEventChangedEntries = changedEntry.ChangedEntries.ToList(); });
+
+            var changedEventChangedEntries = new List<GenericChangedEntry<CatalogProduct>>();
+            AssignChangedEntriesToLicalVariable<ProductChangedEvent, CatalogProduct>(
+                eventPublisher,
+                (changedEntry, token) => { changedEventChangedEntries = changedEntry.ChangedEntries.ToList(); });
+
+            var itemService = GetItemService(catalogRepo.Object, commerceService.Object, outlineService.Object,
+                catalogService.Object, categoryService.Object, GetValidator(), eventPublisher.Object);
+
+            itemService.Update(new[] {catalogProduct});
+
+            AssertValues<ProductChangingEvent, CatalogProduct>(eventPublisher, changingEventChangedEntries, EntryState.Modified);
+            AssertValues<ProductChangedEvent, CatalogProduct>(eventPublisher, changedEventChangedEntries, EntryState.Modified);
         }
 
         [Fact]
@@ -71,7 +128,37 @@ namespace VirtoCommerce.CatalogModule.Test.TestEventsPublishing
 
         private CatalogProduct GetCatalogProduct()
         {
-            return new CatalogProduct();
+            return new CatalogProduct
+            {
+                Id = "testCatalogProductId",
+                CatalogId = "testCatalogId",
+                Name = "testproductName",
+                Code = "testCode",
+                Category = new Category
+                {
+                    Properties = new[]
+                    {
+                        new Property(), 
+                    }
+                }
+            };
+        }
+
+        private Catalog GetCatalog()
+        {
+            return new Catalog
+            {
+                Id = "testCatalogId",
+                Name = "testCatalogName"
+            };
+        }
+
+        private ItemEntity GetItemEntity()
+        {
+            return new ItemEntity
+            {
+                Id = "testCatalogProductId"
+            };
         }
     }
 }
