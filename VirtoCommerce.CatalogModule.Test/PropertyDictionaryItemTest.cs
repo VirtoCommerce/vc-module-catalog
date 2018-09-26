d15b57feaaa7b07d302c92c13aa6536e63182888
Eugeny Tatarincev <et@virtoway.com>
2018-09-26 11:46:53 +0200

    Fixed test

--- VirtoCommerce.CatalogModule.Test/PropertyDictionaryItemTest.cs
+++ VirtoCommerce.CatalogModule.Test/PropertyDictionaryItemTest.cs
@@ -5,6 +5,7 @@ using VirtoCommerce.CatalogModule.Data.Services;
 using VirtoCommerce.Domain.Catalog.Model;
 using VirtoCommerce.Domain.Catalog.Model.Search;
 using VirtoCommerce.Domain.Catalog.Services;
+using VirtoCommerce.Domain.Commerce.Model.Search;
 using Xunit;
 
 
@@ -17,8 +18,8 @@ namespace VirtoCommerce.CatalogModule.Test
         public void Add_New_DictionaryItems_To_Property_And_Then_Choose_DictItem_For_Product()
         {
             var propDictionaryService = new Moq.Mock<IProperyDictionaryItemService>().Object;
-            var propDictionarySearchService = new Moq.Mock<IProperyDictionaryItemSearchService>().Object;
-            var productService = new Moq.Mock<IItemService>().Object;
+            var propDictionarySearchService = new Moq.Mock<IProperyDictionaryItemSearchService>();
+            var productService = new Moq.Mock<IItemService>();
 
             var colorProperty = new Property
             {
@@ -42,15 +43,20 @@ namespace VirtoCommerce.CatalogModule.Test
                         new PropertyDictionaryItemLocalizedValue { LanguageCode = "de", Value = "grün"  }
                    }
             };
+            propDictionarySearchService.Setup(x => x.Search(It.IsAny<PropertyDictionaryItemSearchCriteria>())).Returns(new GenericSearchResult<PropertyDictionaryItem> { TotalCount = 1, Results = new[] { greenDictItem } });
+            productService.Setup(x => x.GetById(It.IsAny<string>(), It.IsAny<ItemResponseGroup>(), null)).Returns(new CatalogProduct { PropertyValues = new List<PropertyValue>() });
             //Add the new dictionary item to the property
             propDictionaryService.SaveChanges(new[] { greenDictItem });
 
-            var product = productService.GetById("Shoes", ItemResponseGroup.ItemProperties);
+
+            var product = productService.Object.GetById("Shoes", ItemResponseGroup.ItemProperties);
             //Find the desired dictionary value from all available
-            greenDictItem = propDictionarySearchService.Search(new PropertyDictionaryItemSearchCriteria { PropertyIds = new[] { colorProperty.Id }, SearchPhrase = "Green" }).Results.FirstOrDefault();
+            greenDictItem = propDictionarySearchService.Object.Search(new PropertyDictionaryItemSearchCriteria { PropertyIds = new[] { colorProperty.Id }, SearchPhrase = "Green" }).Results.FirstOrDefault();
             //Choose dictionary item for product property
             product.PropertyValues.Add(new PropertyValue { Alias = greenDictItem.Alias, PropertyId = greenDictItem.PropertyId, ValueId = greenDictItem.Id });
-            productService.Update(new[] { product });
+            productService.Object.Update(new[] { product });
+
+
         }
 
 
