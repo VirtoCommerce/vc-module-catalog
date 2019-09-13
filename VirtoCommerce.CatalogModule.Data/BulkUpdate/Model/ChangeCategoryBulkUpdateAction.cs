5a5a50204f6c03f56103f12031f258f805636ea8
alexandr andreev <alivemenone@gmail.com>
2019-09-13 12:55:11 +0300

    VP-422: Add change category action (#255)
    Add change category action

---
+++ VirtoCommerce.CatalogModule.Data/BulkUpdate/Model/ChangeCategoryBulkUpdateAction.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VirtoCommerce.Domain.Catalog.Model;
+using VirtoCommerce.Domain.Catalog.Services;
+using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.CatalogModule.Data.BulkUpdate.Model
+{
+    public class ChangeCategoryBulkUpdateAction : IBulkUpdateAction
+    {
+        private readonly IItemService _itemService;
+        private readonly ICatalogService _catalogService;
+
+        public ChangeCategoryBulkUpdateAction(IItemService itemService, ICatalogService catalogService)
+        {
+            _itemService = itemService;
+            _catalogService = catalogService;
+        }
+
+        public BulkUpdateActionContext Context { get; }
+        public IBulkUpdateActionData GetActionData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BulkUpdateActionResult Validate()
+        {
+            var result = BulkUpdateActionResult.Success;
+            if (Context is ChangeCategoryActionContext changeCategoryContext)
+            {
+                var dstCatalog = _catalogService.GetById(changeCategoryContext.CatalogId);
+                if (dstCatalog.IsVirtual)
+                {
+                    result.Succeeded = false;
+                    result.Errors.Add("Unable to move in virtual catalog");
+                }
+            }
+            else
+            {
+                throw new InvalidCastException(nameof(ChangeCategoryActionContext));
+            }
+            return result;
+        }
+
+        public BulkUpdateActionResult Execute(IEnumerable<IEntity> entities)
+        {
+            var result = BulkUpdateActionResult.Success;
+
+            var changeCategoryContext = Context as ChangeCategoryActionContext;
+
+            if (changeCategoryContext == null)
+            {
+                throw new InvalidCastException(nameof(ChangeCategoryActionContext));
+            }
+
+            var products = new List<CatalogProduct>();
+            //Move products
+            foreach (var listEntryProduct in entities)
+            {
+                var product = _itemService.GetById(listEntryProduct.Id, ItemResponseGroup.ItemLarge);
+                if (product.CatalogId != changeCategoryContext.CatalogId)
+                {
+                    product.CatalogId = changeCategoryContext.CatalogId;
+                    product.CategoryId = null;
+                    foreach (var variation in product.Variations)
+                    {
+                        variation.CatalogId = changeCategoryContext.CatalogId;
+                        variation.CategoryId = null;
+                    }
+
+                }
+                if (product.CategoryId != changeCategoryContext.CategoryId)
+                {
+                    product.CategoryId = changeCategoryContext.CategoryId;
+                    foreach (var variation in product.Variations)
+                    {
+                        variation.CategoryId = changeCategoryContext.CategoryId;
+                    }
+                }
+                products.Add(product);
+                products.AddRange(product.Variations);
+            }
+
+            if (products.Any())
+            {
+                _itemService.Update(products.ToArray());
+            }
+
+            return result;
+        }
+    }
+}
