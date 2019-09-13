042d73253133c0919bb45a8dd5a14d894f77fbdb
Aleksandr Andreev <alivemenone@gmail.com>
2019-09-13 12:47:24 +0300

    add change category action

--- VirtoCommerce.CatalogModule.Data/BulkUpdate/Model/BulkUpdateActionResult.cs
+++ VirtoCommerce.CatalogModule.Data/BulkUpdate/Model/BulkUpdateActionResult.cs
@@ -4,7 +4,14 @@ namespace VirtoCommerce.CatalogModule.Data.BulkUpdate.Model
 {
     public class BulkUpdateActionResult
     {
+        public BulkUpdateActionResult()
+        {
+            Errors = new List<string>();
+        }
+
         public bool Succeeded { get; set; }
         public List<string> Errors { get; set; }
+
+        public static BulkUpdateActionResult Success => new BulkUpdateActionResult() { Succeeded = true };
     }
 }
