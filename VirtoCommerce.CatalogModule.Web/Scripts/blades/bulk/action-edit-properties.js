060657c86f4b0d79d7de34cfb29177b4de9c2680
alexandr andreev <alivemenone@gmail.com>
2019-09-17 18:03:20 +0300

    VP-421: update_properties_UI (#261)

--- VirtoCommerce.CatalogModule.Web/Scripts/blades/bulk/action-edit-properties.js
+++ VirtoCommerce.CatalogModule.Web/Scripts/blades/bulk/action-edit-properties.js
@@ -24,6 +24,16 @@ angular.module('virtoCommerce.catalogModule')
                     blade.properties = data.properties;
                     _.each(blade.properties,
                         function (prop) {
+                            if (!prop.id &&
+                            (prop.name === 'Vendor' ||
+                                prop.name === 'TaxType' ||
+                                prop.name === 'MeasureUnit' ||
+                                prop.name === 'PackageType' ||
+                                prop.name === 'WeightUnit')) {
+                                prop.UseDefaultUIForEdit = false;
+                            } else {
+                                prop.UseDefaultUIForEdit = true;
+                            }
                             prop.values = [];
                         });
                 });
@@ -69,7 +79,7 @@ angular.module('virtoCommerce.catalogModule')
                 controller: 'virtoCommerce.catalogModule.editPropertiesActionStepController',
                 template: 'Modules/$(VirtoCommerce.Catalog)/Scripts/blades/bulk/step-edit-properties.tpl.html',
                 properties: blade.includedProperties,
-                propGroups: [{ title: 'Product fields', type: 'own' },{ title: 'catalog.properties.product', type: 'Product' }, { title: 'catalog.properties.variation', type: 'Variation' }],
+                propGroups: [{ title: 'catalog.properties.product', type: 'Product' }, { title: 'catalog.properties.variation', type: 'Variation' }],
                 onSelected: function (editedProperties) {
                     blade.canStartProcess = true;
                     blade.actionDataContext.properties = editedProperties;
