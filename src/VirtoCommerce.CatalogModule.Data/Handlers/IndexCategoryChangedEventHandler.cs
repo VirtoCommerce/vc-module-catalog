6b80bb5b2dcf6a8ac9b62189ade724ec639dfe85
Andrei Kostyrin <kostyrin2@gmail.com>
2019-10-22 12:09:45 +0200

    #vp-732 IndexingJobs

--- src/VirtoCommerce.CatalogModule.Data/Handlers/IndexCategoryChangedEventHandler.cs
+++ src/VirtoCommerce.CatalogModule.Data/Handlers/IndexCategoryChangedEventHandler.cs
@@ -1,25 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hangfire;
 using VirtoCommerce.CatalogModule.Core.Events;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.SearchModule.Core.Model;
-using VirtoCommerce.SearchModule.Core.Services;
+using VirtoCommerce.SearchModule.Data.BackgroundJobs;
 
 namespace VirtoCommerce.CatalogModule.Data.Handlers
 {
     public class IndexCategoryChangedEventHandler : IEventHandler<CategoryChangedEvent>
     {
-        private readonly IIndexingManager _indexingManager;
-
-        public IndexCategoryChangedEventHandler(IIndexingManager indexingManager)
-        {
-            _indexingManager = indexingManager;
-        }
-
         public Task Handle(CategoryChangedEvent message)
         {
             if (message == null)
@@ -27,48 +17,13 @@ namespace VirtoCommerce.CatalogModule.Data.Handlers
                 throw new ArgumentNullException(nameof(message));
             }
 
-            var indexCategoryIds = message.ChangedEntries.Where(x => (x.EntryState == EntryState.Modified || x.EntryState == EntryState.Added) && x.OldEntry.Id != null)
-                                                          .Select(x => x.OldEntry.Id)
-                                                          .Distinct().ToArray();
+            var indexEntries = message.ChangedEntries
+                .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Category })
+                .ToArray();
 
-            if (!indexCategoryIds.IsNullOrEmpty())
-            {
-                BackgroundJob.Enqueue(() => TryIndexCategoryBackgroundJob(indexCategoryIds));
-            }
-
-            var deleteIndexCategoryIds = message.ChangedEntries.Where(x => x.EntryState == EntryState.Deleted && x.OldEntry.Id != null)
-                                                          .Select(x => x.OldEntry.Id)
-                                                          .Distinct().ToArray();
-
-            if (!deleteIndexCategoryIds.IsNullOrEmpty())
-            {
-                BackgroundJob.Enqueue(() => TryDeleteIndexCategoryBackgroundJob(deleteIndexCategoryIds));
-            }
+            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
 
             return Task.CompletedTask;
-        }
-
-        [DisableConcurrentExecution(60 * 60 * 24)]
-        public async Task TryIndexCategoryBackgroundJob(string[] indexCategoryIds)
-        {
-            await TryIndexCategory(indexCategoryIds);
-        }
-
-        [DisableConcurrentExecution(60 * 60 * 24)]
-        public async Task TryDeleteIndexCategoryBackgroundJob(string[] indexCategoryIds)
-        {
-            await TryDeleteIndexCategory(indexCategoryIds);
-        }
-
-
-        protected virtual async Task TryIndexCategory(string[] indexCategoryIds)
-        {
-            await _indexingManager.IndexDocumentsAsync(KnownDocumentTypes.Category, indexCategoryIds);
-        }
-
-        protected virtual async Task TryDeleteIndexCategory(string[] indexCategoryIds)
-        {
-            await _indexingManager.DeleteDocumentsAsync(KnownDocumentTypes.Category, indexCategoryIds);
         }
     }
 }
--- src/VirtoCommerce.CatalogModule.Data/Handlers/IndexProductChangedEventHandler.cs
+++ src/VirtoCommerce.CatalogModule.Data/Handlers/IndexProductChangedEventHandler.cs
@@ -1,73 +1,29 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hangfire;
 using VirtoCommerce.CatalogModule.Core.Events;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.SearchModule.Core.Model;
-using VirtoCommerce.SearchModule.Core.Services;
+using VirtoCommerce.SearchModule.Data.BackgroundJobs;
 
 namespace VirtoCommerce.CatalogModule.Data.Handlers
 {
     public class IndexProductChangedEventHandler : IEventHandler<ProductChangedEvent>
     {
-        private readonly IIndexingManager _indexingManager;
-
-        public IndexProductChangedEventHandler(IIndexingManager indexingManager)
-        {
-            _indexingManager = indexingManager;
-        }
-
-        public async Task Handle(ProductChangedEvent message)
+        public Task Handle(ProductChangedEvent message)
         {
             if (message == null)
             {
                 throw new ArgumentNullException(nameof(message));
             }
 
-            var indexProductIds = message.ChangedEntries.Where(x => (x.EntryState == EntryState.Modified || x.EntryState == EntryState.Added) && x.OldEntry.Id != null)
-                                                          .Select(x => x.OldEntry.Id)
-                                                          .Distinct().ToArray();
+            var indexEntries = message.ChangedEntries
+                .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Product })
+                .ToArray();
 
-            if (!indexProductIds.IsNullOrEmpty())
-            {
-                await TryIndexIndexCategoryBackgroundJob(indexProductIds);
-            }
+            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
 
-            var deleteIndexProductIds = message.ChangedEntries.Where(x => x.EntryState == EntryState.Deleted && x.OldEntry.Id != null)
-                                                          .Select(x => x.OldEntry.Id)
-                                                          .Distinct().ToArray();
-
-            if (!deleteIndexProductIds.IsNullOrEmpty())
-            {
-                await TryDeleteIndexIndexCategoryBackgroundJob(deleteIndexProductIds);
-            }
-        }
-
-
-        [DisableConcurrentExecution(60 * 60 * 24)]
-        public async Task TryIndexIndexCategoryBackgroundJob(string[] indexProductIds)
-        {
-            await TryIndexIndexCategory(indexProductIds);
-        }
-
-        [DisableConcurrentExecution(60 * 60 * 24)]
-        public async Task TryDeleteIndexIndexCategoryBackgroundJob(string[] deleteIndexProductIds)
-        {
-            await TryDeleteIndexIndexCategory(deleteIndexProductIds);
-        }
-
-
-        protected virtual async Task TryIndexIndexCategory(string[] indexProductIds)
-        {
-            await _indexingManager.IndexDocumentsAsync(KnownDocumentTypes.Product, indexProductIds);
-        }
-
-        protected virtual async Task TryDeleteIndexIndexCategory(string[] deleteIndexProductIds)
-        {
-            await _indexingManager.DeleteDocumentsAsync(KnownDocumentTypes.Product, deleteIndexProductIds);
+            return Task.CompletedTask;
         }
     }
 }
