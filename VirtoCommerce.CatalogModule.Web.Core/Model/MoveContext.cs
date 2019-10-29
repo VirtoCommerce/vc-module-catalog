65cc258304d1bbcbbea1a578fdca886634d94ad6
Timofey <tburashov@gmail.com>
2019-10-29 10:11:01 +0700

    VP-773: move ListEntry models to level higher

--- VirtoCommerce.CatalogModule.Web.Core/Model/MoveContext.cs
+++ VirtoCommerce.CatalogModule.Web.Core/Model/MoveContext.cs
@@ -1,16 +1,11 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace VirtoCommerce.CatalogModule.Web.Model
 {
-    /// <summary>
-    /// Represent move operation detail
-    /// </summary>
-    public class MoveInfo
+    public class MoveContext
     {
         public string Catalog { get; set; }
+
         public string Category { get; set; }
 
         public ICollection<ListEntry> ListEntries { get; set; }
