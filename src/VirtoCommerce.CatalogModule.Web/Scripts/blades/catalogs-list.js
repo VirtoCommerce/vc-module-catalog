207e269bbfb8e6b4a9b71b62672ec4fd8e4452b8
Aleksandr Andreev <alivemenone@gmail.com>
2020-04-21 12:06:43 +0300

    Fix 'Count' label and refresh button behavior

--- src/VirtoCommerce.CatalogModule.Web/Scripts/blades/catalogs-list.js
+++ src/VirtoCommerce.CatalogModule.Web/Scripts/blades/catalogs-list.js
@@ -7,7 +7,7 @@ angular.module('virtoCommerce.catalogModule')
             blade.currentEntities = [];
             var selectedNode = null;
 
-            blade.refresh = function() {
+            blade.refresh = function () {
                 blade.isLoading = true;
 
                 if ($scope.pageSettings.currentPage !== 1)
@@ -15,7 +15,7 @@ angular.module('virtoCommerce.catalogModule')
 
                 catalogs.search(getSearchCriteria(), function (data) {
                     blade.isLoading = false;
-                    $scope.pageSettings.totalItems = data.results.length;
+                    $scope.pageSettings.totalItems = data.totalCount;
                     blade.currentEntities = data.results;
                     $scope.hasMore = data.results.length === $scope.pageSettings.itemsPerPageCount;
 
@@ -23,13 +23,12 @@ angular.module('virtoCommerce.catalogModule')
                         //select the node in the new list
                         selectedNode = _.findWhere(data.results, { id: selectedNode.id });
                     }
-
-                    if ($scope.gridApi) {
-                        $scope.gridApi.selection.clearSelectedRows();
-                        $scope.gridApi.infiniteScroll.resetScroll(true, true);
-                        $scope.gridApi.infiniteScroll.dataLoaded();
-                    }
                 });
+                if ($scope.gridApi) {
+                    blade.currentEntities = [];
+                    $scope.gridApi.infiniteScroll.resetScroll(true, true);
+                    $scope.gridApi.infiniteScroll.dataLoaded();
+                }
             };
 
             function showMore() {
@@ -56,7 +55,7 @@ angular.module('virtoCommerce.catalogModule')
                 };
             }
 
-            $scope.selectNode = function(node) {
+            $scope.selectNode = function (node) {
                 selectedNode = node;
                 $scope.selectedNodeId = selectedNode.id;
 
@@ -97,7 +96,7 @@ angular.module('virtoCommerce.catalogModule')
                 bladeNavigationService.showBlade(newBlade, blade);
             }
 
-            $scope.editCatalog = function(catalog) {
+            $scope.editCatalog = function (catalog) {
                 if (catalog.isVirtual) {
                     showVirtualCatalogBlade(catalog.id, null, catalog.name);
                 } else {
@@ -105,20 +104,20 @@ angular.module('virtoCommerce.catalogModule')
                 }
             };
 
-            $scope.deleteCatalog = function(node) {
+            $scope.deleteCatalog = function (node) {
                 var dialog = {
                     id: "confirmDelete",
                     name: node.name,
-                    callback: function(remove) {
+                    callback: function (remove) {
                         if (remove) {
                             bladeNavigationService.closeChildrenBlades(blade,
-                                function() {
+                                function () {
                                     selectedNode = undefined;
                                     $scope.selectedNodeId = undefined;
                                     blade.isLoading = true;
                                     catalogs.delete({ id: node.id },
                                         blade.refresh,
-                                        function(error) {
+                                        function (error) {
                                             bladeNavigationService.setError('Error ' + error.status, blade);
                                         }
                                     );
@@ -158,102 +157,79 @@ angular.module('virtoCommerce.catalogModule')
 
                 bladeNavigationService.showBlade(newBlade, blade);
             }
+
+            blade.toolbarCommands = [
+                {
+                    name: "platform.commands.refresh",
+                    icon: 'fa fa-refresh',
+                    executeMethod: blade.refresh,
+                    canExecuteMethod: function () {
+                        return true;
+                    }
                 }
+            ];
+
+            if (authService.checkPermission('catalog:create')) {
+                blade.toolbarCommands.splice(1,
+                    0,
+                    {
+                        name: "platform.commands.add",
+                        icon: 'fa fa-plus',
+                        executeMethod: function () {
+                            selectedNode = undefined;
+                            $scope.selectedNodeId = undefined;
+
+                            var newBlade = {
+                                id: 'listItemChild',
+                                title: 'catalog.blades.catalog-add.title',
+                                subtitle: 'catalog.blades.catalog-add.subtitle',
+                                controller: 'virtoCommerce.catalogModule.catalogAddController',
+                                template: 'Modules/$(VirtoCommerce.Catalog)/Scripts/blades/catalog-add.tpl.html'
+                            };
+
+                            bladeNavigationService.showBlade(newBlade, blade);
+                        },
+                        canExecuteMethod: function () {
+                            return true;
+                        }
+                    });
             }
-        };
-        dialogService.showDialog(dialog, 'Modules/$(VirtoCommerce.Catalog)/Scripts/dialogs/deleteCatalog-dialog.tpl.html', 'platformWebApp.confirmDialogController');
-    };
 
-    function showCatalogBlade(id, data, title) {
-        var newBlade = {
-            currentEntityId: id,
-            currentEntity: data,
-            title: title,
-            id: 'catalogEdit',
-            subtitle: 'catalog.blades.catalog-detail.subtitle',
-            controller: 'virtoCommerce.catalogModule.catalogDetailController',
-            template: 'Modules/$(VirtoCommerce.Catalog)/Scripts/blades/catalog-detail.tpl.html',
-        };
+            // simple and advanced filtering
+            var filter = blade.filter = { keyword: null };
 
-        bladeNavigationService.showBlade(newBlade, blade);
-    }
+            filter.criteriaChanged = function () {
+                if (filter.keyword) {
+                    openItemsBlade({});
+                } else
+                    bladeNavigationService.closeChildrenBlades(blade);
 
-    function showVirtualCatalogBlade(id, data, title) {
-        var newBlade = {
-            currentEntityId: id,
-            currentEntity: data,
-            title: title,
-            subtitle: 'catalog.blades.catalog-detail.subtitle-virtual',
-            id: 'catalogEdit',
-            controller: 'virtoCommerce.catalogModule.virtualCatalogDetailController',
-            template: 'Modules/$(VirtoCommerce.Catalog)/Scripts/blades/catalog-detail.tpl.html',
-        };
+                selectedNode = null;
+                $scope.selectedNodeId = null;
+                bladeNavigationService.catalogsSelectedCatalog = undefined;
+                bladeNavigationService.catalogsSelectedCategoryId = undefined;
+            };
 
-        bladeNavigationService.showBlade(newBlade, blade);
-    }
+            $scope.setGridOptions = function (gridOptions) {
+                bladeUtils.initializePagination($scope, true);
+                $scope.pageSettings.itemsPerPageCount = 20;
 
-    blade.toolbarCommands = [
-    {
-        name: "platform.commands.refresh", icon: 'fa fa-refresh',
-        executeMethod: blade.refresh,
-        canExecuteMethod: function () {
-            return true;
+                uiGridHelper.initialize($scope, gridOptions, function (gridApi) {
+                    //update gridApi for current grid
+                    $scope.gridApi = gridApi;
+
+                    uiGridHelper.bindRefreshOnSortChanged($scope);
+                    $scope.gridApi.infiniteScroll.on.needLoadMoreData($scope, showMore);
+
+                });
+
+                // need to call refresh after digest cycle as we do not "$watch" for $scope.pageSettings.currentPage
+                $timeout(function () {
+                    blade.refresh();
+                });
+            };
+
+            // actions on load
+            // blade.refresh();
         }
-    }
-    ];
-
-    if (authService.checkPermission('catalog:create')) {
-        blade.toolbarCommands.splice(1, 0, {
-            name: "platform.commands.add",
-            icon: 'fa fa-plus',
-            executeMethod: function () {
-                selectedNode = undefined;
-                $scope.selectedNodeId = undefined;
-
-                var newBlade = {
-                    id: 'listItemChild',
-                    title: 'catalog.blades.catalog-add.title',
-                    subtitle: 'catalog.blades.catalog-add.subtitle',
-                    controller: 'virtoCommerce.catalogModule.catalogAddController',
-                    template: 'Modules/$(VirtoCommerce.Catalog)/Scripts/blades/catalog-add.tpl.html'
-                };
-
-                bladeNavigationService.showBlade(newBlade, blade);
-            },
-            canExecuteMethod: function () {
-                return true;
-            }
-        });
-    }
-
-    // simple and advanced filtering
-    var filter = blade.filter = { keyword: null };
-
-    filter.criteriaChanged = function () {
-        if (filter.keyword) {
-            openItemsBlade({});
-        } else
-            bladeNavigationService.closeChildrenBlades(blade);
-
-        selectedNode = null;
-        $scope.selectedNodeId = null;
-        bladeNavigationService.catalogsSelectedCatalog = undefined;
-        bladeNavigationService.catalogsSelectedCategoryId = undefined;
-    };
-
-    // ui-grid
-    $scope.setGridOptions = function (gridOptions) {
-        $scope.gridOptions = gridOptions;
-        uiGridHelper.initialize($scope, gridOptions, function (gridApi) {
-            //update gridApi for current grid
-            $scope.gridApi = gridApi;
-
-            uiGridHelper.bindRefreshOnSortChanged($scope);
-        });
-        bladeUtils.initializePagination($scope);
-        return gridOptions;
-    };
-
-    // actions on load
-    // blade.refresh();
-}]);
+    ]);
