ec3b4de67e6711a62985c34c559b8c264f359aef
Andrei Kolchanov <akak1977@gmail.com>
2020-05-13 18:35:04 +0500

    Add clone Catalog, Category tests

--- tests/VirtoCommerce.CatalogModule.Tests/EntityCloningTests.cs
+++ tests/VirtoCommerce.CatalogModule.Tests/EntityCloningTests.cs
@@ -59,5 +59,58 @@ namespace VirtoCommerce.CatalogModule.Test
                 Assert.Equal(sCatalogProduct1st, sCatalogProduct2nd);
             });
         }
+
+        [Fact]
+        public async Task CloneCatalog()
+        {
+            A.Configure<Catalog>()
+                .Fill(x => x.Languages, A.ListOf<CatalogLanguage>(3))
+                .Fill(x => x.Properties, A.ListOf<Property>(10));
+            var catalog = A.New<Catalog>();
+
+            await Task.Run(() =>
+            {
+                var sCatalog1st = JsonSerializer.Serialize(catalog, new JsonSerializerOptions() { WriteIndented = true });
+                var sCatalog2nd = JsonSerializer.Serialize(catalog.Clone(), new JsonSerializerOptions() { WriteIndented = true });
+                Assert.Equal(sCatalog1st, sCatalog2nd);
+            });
+        }
+
+        [Fact]
+        public async Task CloneCategory()
+        {
+            A.Configure<Category>()
+                .Fill(x => x.Images, A.ListOf<Image>(10))
+                .Fill(x => x.Links, A.ListOf<CategoryLink>(10))
+                .Fill(x => x.Outlines, A.ListOf<Outline>(5))
+                .Fill(x => x.Properties, A.ListOf<Property>(10))
+                .Fill(x => x.SeoInfos, A.ListOf<SeoInfo>(10));
+
+            A.Configure<Catalog>()
+                .Fill(x => x.Languages, A.ListOf<CatalogLanguage>(3))
+                .Fill(x => x.Properties, A.ListOf<Property>(10));
+
+            A.Configure<OutlineItem>()
+                .Fill(x => x.SeoInfos, A.ListOf<SeoInfo>(5));
+
+            A.Configure<Outline>()
+                .Fill(x => x.Items, A.ListOf<OutlineItem>(5));
+
+            A.Configure<Property>()
+                .Fill(x => x.Attributes, A.ListOf<PropertyAttribute>(5))
+                .Fill(x => x.DisplayNames, A.ListOf<PropertyDisplayName>(5))
+                .Fill(x => x.ValidationRules, A.ListOf<PropertyValidationRule>(5))
+                .Fill(x => x.Values, A.ListOf<PropertyValue>(5));
+
+            var category = A.New<Category>();
+            category.Parents = A.ListOf<Category>(1).ToArray();
+
+            await Task.Run(() =>
+            {
+                var sCategory1st = JsonSerializer.Serialize(category, new JsonSerializerOptions() { WriteIndented = true });
+                var sCategory2st = JsonSerializer.Serialize(category.Clone(), new JsonSerializerOptions() { WriteIndented = true });
+                Assert.Equal(sCategory1st, sCategory2st);
+            });
+        }
     }
 }
