25ae91cc40adebcf8d107ec0bf38519b19eaa1bc
Egidijus Mazeika <em@virtoway.com>
2020-05-19 22:22:17 +0300

    Apply EventBasedIndexation setting value immediately #VP-2400

--- src/VirtoCommerce.CatalogModule.Data/Handlers/IndexProductChangedEventHandler.cs
+++ src/VirtoCommerce.CatalogModule.Data/Handlers/IndexProductChangedEventHandler.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using VirtoCommerce.CatalogModule.Core;
 using VirtoCommerce.CatalogModule.Core.Events;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
 
@@ -10,20 +12,28 @@ namespace VirtoCommerce.CatalogModule.Data.Handlers
 {
     public class IndexProductChangedEventHandler : IEventHandler<ProductChangedEvent>
     {
-        public Task Handle(ProductChangedEvent message)
+        private readonly ISettingsManager _settingsManager;
+
+        public IndexProductChangedEventHandler(ISettingsManager settingsManager)
         {
-            if (message == null)
+            _settingsManager = settingsManager;
+        }
+
+        public async Task Handle(ProductChangedEvent message)
+        {
+            if (await _settingsManager.GetValueAsync(ModuleConstants.Settings.General.EventBasedIndexation.Name, false))
             {
-                throw new ArgumentNullException(nameof(message));
+                if (message == null)
+                {
+                    throw new ArgumentNullException(nameof(message));
+                }
+
+                var indexEntries = message.ChangedEntries
+                    .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Product })
+                    .ToArray();
+
+                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
             }
-
-            var indexEntries = message.ChangedEntries
-                .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Product })
-                .ToArray();
-
-            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
-
-            return Task.CompletedTask;
         }
     }
 }
