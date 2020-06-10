44a697754703b0fde0fd5de284cc3d314856949e
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2020-06-10 12:29:19 +0200

    Add dynamic association search cache region

---
+++ src/VirtoCommerce.CatalogModule.Data/Caching/DynamicAssociationSearchCacheRegion.cs
@@ -0,0 +1,8 @@
+using VirtoCommerce.Platform.Core.Caching;
+
+namespace VirtoCommerce.CatalogModule.Data.Caching
+{
+    public class DynamicAssociationSearchCacheRegion : CancellableCacheRegion<DynamicAssociationSearchCacheRegion>
+    {
+    }
+}
