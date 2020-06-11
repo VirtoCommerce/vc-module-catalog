088a1df92121359b4f516117375dd923ec08160d
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2020-06-11 08:23:27 +0200

    Add nullable serialized expression tree migration

---
+++ src/VirtoCommerce.CatalogModule.Data/Migrations/20200611061908_ExpressionTreeSerializedNullable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.CatalogModule.Data.Migrations
+{
+    public partial class ExpressionTreeSerializedNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpressionTreeSerialized",
+                table: "DynamicAssociation",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpressionTreeSerialized",
+                table: "DynamicAssociation",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
