7d265a928bfbc7902a9a54dcaecd2d458b6cb5f7
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2020-06-16 17:43:36 +0200

    Refactoring DTO

--- src/VirtoCommerce.CatalogModule.Core/Model/Search/ProductsToMatchSearchContext.cs
+++ src/VirtoCommerce.CatalogModule.Core/Model/Search/ProductsToMatchSearchContext.cs
@@ -4,19 +4,14 @@ namespace VirtoCommerce.CatalogModule.Core.Model.Search
 {
     public class ProductsToMatchSearchContext : ValueObject
     {
-        public ProductsToMatchSearchContext()
-        {
-            Take = 10;
-        }
-
         public string StoreId { get; set; }
 
         public string[] ProductsToMatch { get; set; }
 
         public string Group { get; set; }
 
-        public int? Take { get; set; }
+        public int Take { get; set; } = 20;
 
-        public int? Skip { get; set; }
+        public int Skip { get; set; } = 0;
     }
 }
