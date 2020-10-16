5fe9294479ac67ea7725680b8d0985e160c9c44f
Eugeny Tatarincev <et@virtoway.com>
2020-10-16 09:28:58 +0200

    VP-5366 Fix for unable to remove value for property if it is single in the property set

--- src/VirtoCommerce.CatalogModule.Data/Model/ItemEntity.cs
+++ src/VirtoCommerce.CatalogModule.Data/Model/ItemEntity.cs
@@ -284,18 +284,27 @@ namespace VirtoCommerce.CatalogModule.Data.Model
                 {
                     if (property.Values != null)
                     {
-                        foreach (var propValue in property.Values.Where(x => x != null))
+                        foreach (var propValue in property.Values)
                         {
-                            //Need populate required fields
-                            propValue.PropertyName = property.Name;
-                            propValue.ValueType = property.ValueType;
-                            propValues.Add(propValue);
+                            //Do not use values from inherited properties 
+                            if (propValue != null && !propValue.IsInherited)
+                            {
+                                //Need populate required fields
+                                propValue.PropertyName = property.Name;
+                                propValue.ValueType = property.ValueType;
+                                propValues.Add(propValue);
+                            }
+                            else
+                            {
+                                //Add empty property value for null values to be able remove these values from db in the lines below 
+                                propValues.Add(new PropertyValue());
+                            }
                         }
                     }
                 }
                 if (!propValues.IsNullOrEmpty())
                 {
-                    ItemPropertyValues = new ObservableCollection<PropertyValueEntity>(AbstractTypeFactory<PropertyValueEntity>.TryCreateInstance().FromModels(propValues, pkMap));
+                    ItemPropertyValues = new ObservableCollection<PropertyValueEntity>(AbstractTypeFactory<PropertyValueEntity>.TryCreateInstance().FromModels(propValues.Where(x=> !x.IsEmpty), pkMap));
                 }
             }
             else if (!product.PropertyValues.IsNullOrEmpty())
