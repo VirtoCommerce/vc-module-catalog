506a033811b6d552c492258c6bc8d4a419017df2
Andrey Kozlov <yecli.github@gmail.com>
2020-12-11 19:56:55 +0500

    VP-6350: Throw exception when moving to category to itslef or its children

--- src/VirtoCommerce.CatalogModule.Data/Services/CategoryMover.cs
+++ src/VirtoCommerce.CatalogModule.Data/Services/CategoryMover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,12 +31,29 @@ namespace VirtoCommerce.CatalogModule.Data.Services
 
         public override async Task<List<Category>> PrepareMoveAsync(ListEntriesMoveRequest moveInfo)
         {
+            if (!string.IsNullOrEmpty(moveInfo.Category))
+            {
+                var targetCategory = (await _categoryService.GetByIdsAsync(new[] { moveInfo.Category }, CategoryResponseGroup.WithOutlines.ToString())).FirstOrDefault()
+                    ?? throw new InvalidOperationException($"Destination category does not exist");
+
+                foreach (var listEntryOfTypeCategory in moveInfo.ListEntries.Where(listEntry => listEntry.Type.EqualsInvariant(CategoryListEntry.TypeName)))
+                {
+                    var movedCategoryOutline = string.Join('\\', listEntryOfTypeCategory.Outline);
+
+                    // TODO: Need to think on preventing cycles in the links, and add test
+                    if (targetCategory.Outline.StartsWith(movedCategoryOutline, StringComparison.OrdinalIgnoreCase))
+                    {
+                        throw new InvalidOperationException("Cannot move category under itself");
+                    }
+                }
+            }
+
             var result = new List<Category>();
 
             foreach (var listEntryCategory in moveInfo.ListEntries.Where(
                 listEntry => listEntry.Type.EqualsInvariant(CategoryListEntry.TypeName)))
             {
-                var category = (await _categoryService.GetByIdsAsync(new [] { listEntryCategory.Id }, CategoryResponseGroup.Info.ToString())).FirstOrDefault();
+                var category = (await _categoryService.GetByIdsAsync(new[] { listEntryCategory.Id }, CategoryResponseGroup.Info.ToString())).FirstOrDefault();
                 if (category.CatalogId != moveInfo.Catalog)
                 {
                     category.CatalogId = moveInfo.Catalog;
