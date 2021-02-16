ecc7c4cd9fe6d816905ca8665c93557d0ff95a37
alexandr andreev <aad@virtoway.com>
2021-02-16 12:37:22 +0300

    VP-6852 Add infinite scroll for catalog and priselist selectors (#480)
    * Use already existing ObjectIds for added it SearchCriteria the CatalogIds property.
    * Fix possible null reference exception

--- src/VirtoCommerce.CatalogModule.Core/Model/Search/CatalogSearchCriteria.cs
+++ src/VirtoCommerce.CatalogModule.Core/Model/Search/CatalogSearchCriteria.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Linq;
 using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.CatalogModule.Core.Model.Search
 {
     public class CatalogSearchCriteria : SearchCriteriaBase
     {
-        public string[] CatalogIds { get; set; }
+        public string[] CatalogIds
+        {
+            get => ObjectIds?.ToArray() ?? Array.Empty<string>();
+            set => ObjectIds = value;
+        }
     }
 }
