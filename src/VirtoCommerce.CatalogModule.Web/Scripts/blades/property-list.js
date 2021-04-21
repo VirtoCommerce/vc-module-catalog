5221b54773fa56578435170cc98485261d3e69c9
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-04-21 10:57:52 +0200

    Revert "PT-679 added comment"
    This reverts commit 523358eb42128c1e6e8d584286bb5a24aa00687f.

--- src/VirtoCommerce.CatalogModule.Web/Scripts/blades/property-list.js
+++ src/VirtoCommerce.CatalogModule.Web/Scripts/blades/property-list.js
@@ -239,7 +239,6 @@ angular.module('virtoCommerce.catalogModule')
                     return false;
                 }
 
-                // empty property: no values or all values are null or an empty string 
                 if (!property.values ||
                     !property.values.length ||
                     allPropertiesEmpty(property.values)) {
