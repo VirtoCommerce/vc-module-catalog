cd37108ea43cb64e6ec097c1d2e43070bc255420
Andrei Kolchanov <akak1977@gmail.com>
2021-09-07 23:25:48 +0500

    PT-3497: Make catalog scope picker as a standard multiple selector

--- src/VirtoCommerce.CatalogModule.Web/Scripts/catalog.js
+++ src/VirtoCommerce.CatalogModule.Web/Scripts/catalog.js
@@ -210,7 +210,7 @@ angular.module(catalogsModuleName, ['ui.grid.validate', 'ui.grid.infiniteScroll'
                             subtitle: 'catalog.permissions.catalog-scope.blade.subtitle',
                             currentEntity: this,
                             onChangesConfirmedFn: callback,
-                            dataPromise: catalogs.query().$promise,
+                            dataService: catalogs,
                             controller: 'platformWebApp.security.scopeValuePickFromSimpleListController',
                             template: '$(Platform)/Scripts/app/security/blades/common/scope-value-pick-from-simple-list.tpl.html'
                         };
