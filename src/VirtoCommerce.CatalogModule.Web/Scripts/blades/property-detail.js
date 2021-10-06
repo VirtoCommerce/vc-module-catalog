903d5f4264243ac0913e70023b950b30da1e3f7a
Filip Kuksov <vectorfield4@gmail.com>
2021-10-06 13:25:30 +0200

    Revert "PT-4686: Create multivalue properties for Integer types"
    This reverts commit e898c75f4d1f97aeec686cc3292811597f7f1878.

--- src/VirtoCommerce.CatalogModule.Web/Scripts/blades/property-detail.js
+++ src/VirtoCommerce.CatalogModule.Web/Scripts/blades/property-detail.js
@@ -21,11 +21,11 @@ angular.module('virtoCommerce.catalogModule')
             switch (newValue) {
                 case 'DateTime':
                 case 'Boolean':
+                case 'Integer':
                     blade.hasMultivalue = blade.currentEntity.multivalue = false;
                     blade.hasDictionary = blade.currentEntity.dictionary = false;
                     blade.hasMultilanguage = blade.currentEntity.multilanguage = false;
                     break;
-                case 'Integer':
                 case 'GeoPoint':
                     blade.hasDictionary = blade.currentEntity.dictionary = false;
                     blade.hasMultilanguage = blade.currentEntity.multilanguage = false;
