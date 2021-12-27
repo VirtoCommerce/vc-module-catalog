f5ea816b4dca088b6b939d2b777e59c9684aa0b6
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-12-27 16:03:29 +0200

    Use category iterator

--- src/VirtoCommerce.CatalogModule.Data/Validation/CategoryPropertyNameValidator.cs
+++ src/VirtoCommerce.CatalogModule.Data/Validation/CategoryPropertyNameValidator.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
 using FluentValidation.Results;
 using VirtoCommerce.CatalogModule.Core.Model;
-using VirtoCommerce.CatalogModule.Core.Model.Search;
-using VirtoCommerce.CatalogModule.Core.Search;
 using VirtoCommerce.CatalogModule.Core.Services;
-using VirtoCommerce.CatalogModule.Data.Repositories;
+using VirtoCommerce.CatalogModule.Data.Services;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Settings;
 using catalogCore = VirtoCommerce.CatalogModule.Core;
@@ -19,23 +15,20 @@ namespace VirtoCommerce.CatalogModule.Data.Validation
     {
         private readonly ICategoryService _categoryService;
         private readonly IPropertyService _propertyService;
-        private readonly ICategoryIndexedSearchService _categoryIndexedSearchService;
-        private readonly Func<ICatalogRepository> _catalogRepositoryFactory;
         private readonly ISettingsManager _settingsManager;
+        private readonly CategoryIteratorFactory _categoryIteratorFactory;
 
         public CategoryPropertyNameValidator(
             ICategoryService categoryService,
             IPropertyService propertyService,
-            ICategoryIndexedSearchService categoryIndexedSearchService,
-            Func<ICatalogRepository> catalogRepositoryFactory,
-            ISettingsManager settingsManager
+            ISettingsManager settingsManager,
+            CategoryIteratorFactory categoryIteratorFactory
             )
         {
             _categoryService = categoryService;
             _propertyService = propertyService;
-            _categoryIndexedSearchService = categoryIndexedSearchService;
-            _catalogRepositoryFactory = catalogRepositoryFactory;
             _settingsManager = settingsManager;
+            _categoryIteratorFactory = categoryIteratorFactory;
 
             AttachValidators();
         }
@@ -63,23 +56,36 @@ namespace VirtoCommerce.CatalogModule.Data.Validation
 
         protected virtual async Task<(bool, string)> CheckPropertyUniquenessAsync(CategoryPropertyValidationRequest request)
         {
-            var categoryIds = await GetCategoryIds(request);
+            var categoryIterator = _categoryIteratorFactory.Create(500, request.CatalogId, request.CategoryId);
+            Property existingProperty;
 
-            var childrenCategories = await _categoryService.GetByIdsAsync(categoryIds.ToArray(), CategoryResponseGroup.WithProperties.ToString());
-
-            var properties = childrenCategories.SelectMany(x => x.Properties);
-
-            var useIndexedSearch = await _settingsManager.GetValueAsync(catalogCore.ModuleConstants.Settings.Search.UseCatalogIndexedSearchInManager.Name, true);
-
-            // If useIndexedSearch is off and requested Category is missed, then properties would be empty
-            if (string.IsNullOrEmpty(request.CategoryId) && !useIndexedSearch)
+            do
             {
-                properties = await _propertyService.GetAllCatalogPropertiesAsync(request.CatalogId);
-            }
+                var categoryIds = await categoryIterator.GetNextPageAsync();
 
-            var requiredPropertyType = EnumUtility.SafeParse(request.PropertyType, PropertyType.Category);
+                var childrenCategories = await _categoryService.GetByIdsAsync(categoryIds.ToArray(), CategoryResponseGroup.WithProperties.ToString());
 
-            var existingProperty = properties.FirstOrDefault(x => x.Name.EqualsInvariant(request.PropertyName) && x.Type.Equals(requiredPropertyType));
+                var properties = childrenCategories.SelectMany(x => x.Properties);
+
+                var useIndexedSearch = await _settingsManager.GetValueAsync(catalogCore.ModuleConstants.Settings.Search.UseCatalogIndexedSearchInManager.Name, true);
+
+                // If useIndexedSearch is off and requested Category is missed, then properties would be empty
+                if (string.IsNullOrEmpty(request.CategoryId) && !useIndexedSearch)
+                {
+                    properties = await _propertyService.GetAllCatalogPropertiesAsync(request.CatalogId);
+                }
+
+                var requiredPropertyType = EnumUtility.SafeParse(request.PropertyType, PropertyType.Category);
+
+                existingProperty = properties.FirstOrDefault(x => x.Name.EqualsInvariant(request.PropertyName) && x.Type.Equals(requiredPropertyType));
+
+                if (existingProperty != null)
+                {
+                    break;
+                }
+
+            } while (categoryIterator.HasMoreResults);
+
             var categoryName = string.Empty;
 
             if (existingProperty != null)
@@ -89,43 +95,6 @@ namespace VirtoCommerce.CatalogModule.Data.Validation
             }
 
             return (existingProperty == null, categoryName);
-        }
-
-        protected virtual async Task<string[]> GetCategoryIds(CategoryPropertyValidationRequest request)
-        {
-            var useIndexedSearch = await _settingsManager.GetValueAsync(catalogCore.ModuleConstants.Settings.Search.UseCatalogIndexedSearchInManager.Name, true);
-
-            var categoryIds = new List<string>();
-
-            if (useIndexedSearch)
-            {
-                var indexedSearchCriteria = AbstractTypeFactory<CategoryIndexedSearchCriteria>.TryCreateInstance();
-                indexedSearchCriteria.Outline = GetOutline(request);
-                indexedSearchCriteria.Take = 10000; // Max elastic search batch
-
-                var searchResult = await _categoryIndexedSearchService.SearchAsync(indexedSearchCriteria);
-
-                categoryIds.AddRange(searchResult.Items.Select(x => x.Id));
-            }
-            else if (!string.IsNullOrEmpty(request.CategoryId))
-            {
-                using var catalogRepository = _catalogRepositoryFactory();
-
-                var childrenCategoryIds = await catalogRepository.GetAllChildrenCategoriesIdsAsync(new[] { request.CategoryId });
-
-                categoryIds.AddRange(childrenCategoryIds);
-            }
-
-            return categoryIds.ToArray();
-        }
-
-        protected virtual string GetOutline(CategoryPropertyValidationRequest request)
-        {
-            var result = string.IsNullOrEmpty(request.CategoryId)
-                ? request.CatalogId
-                : $"{request.CatalogId}/{request.CategoryId}";
-
-            return result;
         }
     }
 }
