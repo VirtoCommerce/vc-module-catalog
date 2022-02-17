79ff1178b9afb075e0cfbbd1a4b9eca5f0caad74
getmansky <47969278+getmansky@users.noreply.github.com>
2022-02-17 17:57:28 +0300

    PT-6527: Partial search index update support (#582)
    * Partial index update support
    
    * Using beta search module nuget
    
    * Using release version of the search module Nuget

--- src/VirtoCommerce.CatalogModule.Data/Handlers/IndexCategoryChangedEventHandler.cs
+++ src/VirtoCommerce.CatalogModule.Data/Handlers/IndexCategoryChangedEventHandler.cs
@@ -1,22 +1,28 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.CatalogModule.Core;
 using VirtoCommerce.CatalogModule.Core.Events;
+using VirtoCommerce.CatalogModule.Data.Search.Indexing;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Jobs;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
+using VirtoCommerce.SearchModule.Data.Services;
 
 namespace VirtoCommerce.CatalogModule.Data.Handlers
 {
     public class IndexCategoryChangedEventHandler : IEventHandler<CategoryChangedEvent>
     {
         private readonly ISettingsManager _settingsManager;
+        private readonly IEnumerable<IndexDocumentConfiguration> _configurations;
 
-        public IndexCategoryChangedEventHandler(ISettingsManager settingsManager)
+        public IndexCategoryChangedEventHandler(ISettingsManager settingsManager, IEnumerable<IndexDocumentConfiguration> configurations)
         {
             _settingsManager = settingsManager;
+            _configurations = configurations;
         }
 
         public async Task Handle(CategoryChangedEvent message)
@@ -32,7 +38,8 @@ namespace VirtoCommerce.CatalogModule.Data.Handlers
                     .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Category })
                     .ToArray();
 
-                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
+                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries,
+                    JobPriority.Normal, _configurations.GetBuildersForProvider(typeof(CategoryDocumentChangesProvider)).ToList());
             }
         }
     }
--- src/VirtoCommerce.CatalogModule.Data/Handlers/IndexProductChangedEventHandler.cs
+++ src/VirtoCommerce.CatalogModule.Data/Handlers/IndexProductChangedEventHandler.cs
@@ -1,22 +1,28 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.CatalogModule.Core;
 using VirtoCommerce.CatalogModule.Core.Events;
+using VirtoCommerce.CatalogModule.Data.Search.Indexing;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Jobs;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
+using VirtoCommerce.SearchModule.Data.Services;
 
 namespace VirtoCommerce.CatalogModule.Data.Handlers
 {
     public class IndexProductChangedEventHandler : IEventHandler<ProductChangedEvent>
     {
         private readonly ISettingsManager _settingsManager;
+        private readonly IEnumerable<IndexDocumentConfiguration> _configurations;
 
-        public IndexProductChangedEventHandler(ISettingsManager settingsManager)
+        public IndexProductChangedEventHandler(ISettingsManager settingsManager, IEnumerable<IndexDocumentConfiguration> configurations)
         {
             _settingsManager = settingsManager;
+            _configurations = configurations;
         }
 
         public async Task Handle(ProductChangedEvent message)
@@ -32,7 +38,8 @@ namespace VirtoCommerce.CatalogModule.Data.Handlers
                     .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = KnownDocumentTypes.Product })
                     .ToArray();
 
-                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
+                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries,
+                    JobPriority.Normal, _configurations.GetBuildersForProvider(typeof(ProductDocumentChangesProvider)).ToList());
             }
         }
     }
