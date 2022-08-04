b7d3714789922bbd1f271f7c5e163e0010e5d1c4
VertyR <Roman.Vertyanov@virtoway.com>
2022-08-04 18:53:24 +0600

    feature: PT-4249 Catalog/categories filter. There is no applied filter state.
    #PT-4249 Catalog/categories filter. There is no applied filter state.

---
+++ src/VirtoCommerce.CatalogModule.Web/Scripts/directives/itemSearchCriteria.js
@@ -0,0 +1,15 @@
+angular.module('virtoCommerce.catalogModule').directive('vcItemSearchCriteria', ['$localStorage', 'platformWebApp.bladeNavigationService', 'virtoCommerce.catalogModule.predefinedSearchFilters', function ($localStorage, bladeNavigationService, predefinedSearchFilters) {
+    return {
+        restrict: 'E',
+        templateUrl: function (elem, attrs) {
+            return attrs.templateUrl || 'Modules/$(VirtoCommerce.Catalog)/Scripts/directives/itemSearchCriteria.tpl.html'
+        },
+        scope: {
+            blade: '='
+        },
+        link: function ($scope) {
+            var blade = $scope.blade;
+            var filter = $scope.filter = blade.filter;
+        }
+    }
+}]);
