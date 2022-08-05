fb15392009e74c7b42a6c808d17cacc85b0612a6
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2022-08-05 11:31:29 +0200

    Revert "Delete EnumConvertExtension.cs"
    This reverts commit 02538b33988ecdcf4e8bd1dcf92cc2618f0dddc1.

---
+++ src/VirtoCommerce.CatalogModule.Data/Extensions/EnumConvertExtension.cs
@@ -0,0 +1,20 @@
+using System;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.SearchModule.Core.Model;
+
+namespace VirtoCommerce.CatalogModule.Data.Extensions
+{
+    public static class EnumConvertExtension
+    {
+        public static IndexDocumentChangeType ToIndexDocumentChangeType(this EntryState entryState)
+        {
+            return entryState switch
+            {
+                EntryState.Added => IndexDocumentChangeType.Created,
+                EntryState.Deleted => IndexDocumentChangeType.Deleted,
+                EntryState.Modified => IndexDocumentChangeType.Modified,
+                _ => throw new ArgumentOutOfRangeException(nameof(entryState), entryState, null)
+            };
+        }
+    }
+}
