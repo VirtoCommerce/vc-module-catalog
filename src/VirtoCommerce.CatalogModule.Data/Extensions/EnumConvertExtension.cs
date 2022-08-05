548f3f05a3401616e504168e19f15260ce5e1871
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2022-08-05 11:34:38 +0200

    Revert "fix: Code correction"
    This reverts commit e36b8a58c37ed9bc42d081c44824a921e4f39873.

--- src/VirtoCommerce.CatalogModule.Data/Extensions/EnumConvertExtension.cs
+++ src/VirtoCommerce.CatalogModule.Data/Extensions/EnumConvertExtension.cs
@@ -2,19 +2,19 @@ using System;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.SearchModule.Core.Model;
 
-namespace VirtoCommerce.CatalogModule.Data.Extensions
+namespace VirtoCommerce.CatalogModule.Data.Extensions;
+
+public static class EnumConvertExtension
 {
-    public static class EnumConvertExtension
+    public static IndexDocumentChangeType ToIndexDocumentChangeType(this EntryState entryState)
     {
-        public static IndexDocumentChangeType ToIndexDocumentChangeType(this EntryState entryState)
+        return entryState switch
         {
-            return entryState switch
-            {
-                EntryState.Added => IndexDocumentChangeType.Created,
-                EntryState.Deleted => IndexDocumentChangeType.Deleted,
-                EntryState.Modified => IndexDocumentChangeType.Modified,
-                _ => throw new ArgumentOutOfRangeException(nameof(entryState), entryState, null)
-            };
-        }
+            EntryState.Added => IndexDocumentChangeType.Created,
+            EntryState.Deleted => IndexDocumentChangeType.Deleted,
+            EntryState.Modified => IndexDocumentChangeType.Modified,
+            _ => throw new ArgumentOutOfRangeException(nameof(entryState), entryState, null)
+        };
     }
 }
+
