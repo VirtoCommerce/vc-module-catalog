0c9b231f221c7d5d10cb8d7ad37863b711c39c6e
Oleg Zhuk <zhukoo@gmail.com>
2022-11-14 16:07:01 +0200

    fix: Display incorrect date in Created date for catalog (#638)

--- src/VirtoCommerce.CatalogModule.Data/Model/CatalogEntity.cs
+++ src/VirtoCommerce.CatalogModule.Data/Model/CatalogEntity.cs
@@ -48,6 +48,12 @@ namespace VirtoCommerce.CatalogModule.Data.Model
                 throw new ArgumentNullException(nameof(catalog));
 
             catalog.Id = Id;
+
+            catalog.CreatedDate = CreatedDate;
+            catalog.ModifiedDate = ModifiedDate;
+            catalog.CreatedBy = CreatedBy;
+            catalog.ModifiedBy = ModifiedBy;
+
             catalog.Name = Name;
             catalog.IsVirtual = Virtual;
             catalog.OuterId = OuterId;
@@ -92,6 +98,12 @@ namespace VirtoCommerce.CatalogModule.Data.Model
             pkMap.AddPair(catalog, this);
 
             Id = catalog.Id;
+
+            CreatedDate = catalog.CreatedDate;
+            ModifiedDate = catalog.ModifiedDate;
+            CreatedBy = catalog.CreatedBy;
+            ModifiedBy = catalog.ModifiedBy;
+
             Name = catalog.Name;
             Virtual = catalog.IsVirtual;
             OuterId = catalog.OuterId;
