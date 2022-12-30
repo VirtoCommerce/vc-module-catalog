d735a18661c4592e51d8b8b8bed4a0a0dae4d528
Oleg Zhuk <zhukoo@gmail.com>
2022-12-30 15:12:43 +0200

    Load product properties for Linked Products

--- src/VirtoCommerce.CatalogModule.Web/Controllers/Api/CatalogModuleListEntryController.cs
+++ src/VirtoCommerce.CatalogModule.Web/Controllers/Api/CatalogModuleListEntryController.cs
@@ -307,7 +307,7 @@ namespace VirtoCommerce.CatalogModule.Web.Controllers.Api
         private async Task<IList<T>> LoadCatalogEntriesAsync<T>(string[] ids)
         {
 #pragma warning disable CS0618 // Variations can be used here
-            var products = await _itemService.GetByIdsAsync(ids, (ItemResponseGroup.Links | ItemResponseGroup.Variations).ToString());
+            var products = await _itemService.GetByIdsAsync(ids, (ItemResponseGroup.Links | ItemResponseGroup.ItemProperties | ItemResponseGroup.Variations).ToString());
 #pragma warning restore CS0618
             var categories = await _categoryService.GetAsync(ids.Except(products.Select(x => x.Id)).ToList(), CategoryResponseGroup.WithLinks.ToString());
             return products.OfType<T>().Concat(categories.OfType<T>()).ToList();
