e2070b8c3216d9df9a34a5b05efbd9ce0aa08a29
Oleg Zhuk <zhukoo@gmail.com>
2023-02-28 09:57:38 +0200

    fix code smell

--- src/VirtoCommerce.CatalogModule.Web/webpack.config.js
+++ src/VirtoCommerce.CatalogModule.Web/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = (env, argv) => {
             rules: [
                 {
                     test: /\.css$/,
-                    use: [MiniCssExtractPlugin.loader, "css-loader"] 
+                    use: [MiniCssExtractPlugin.loader, "css-loader"]
                 }
             ]
         },
