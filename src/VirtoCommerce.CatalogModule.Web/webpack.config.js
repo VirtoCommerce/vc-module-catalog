8bd06908d3de86419c19b4ed1d12c4b7f7c919bd
Oleg Zhuk <zhukoo@gmail.com>
2023-03-03 17:51:53 +0200

    Add moduleId, fix code smells.

--- src/VirtoCommerce.CatalogModule.Web/webpack.config.js
+++ src/VirtoCommerce.CatalogModule.Web/webpack.config.js
@@ -1,29 +1,31 @@
+const moduleId = "VirtoCommerce.Catalog"
+
 const glob = require("glob");
 const path = require("path");
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const rootPath = path.resolve(__dirname, 'dist');
+const rootPath = path.resolve(__dirname, "dist");
 
 function getEntrypoints() {
     const result = [
-        ...glob.sync('./Scripts/**/*.js', { nosort: true }),
-        ...glob.sync('./Content/**/*.css', { nosort: true })
+        ...glob.sync("./Scripts/**/*.js", { nosort: true }),
+        ...glob.sync("./Content/**/*.css", { nosort: true })
     ];
 
     return result;
 }
 
 module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
+    const isProduction = argv.mode === "production";
 
     return {
         entry: getEntrypoints(),
         devtool: false,
         output: {
             path: rootPath,
-            filename: 'app.js'
+            filename: "app.js"
         },
         module: {
             rules: [
@@ -37,15 +39,15 @@ module.exports = (env, argv) => {
             new CleanWebpackPlugin(),
             isProduction ? 
                 new webpack.SourceMapDevToolPlugin({
-                    namespace: 'VirtoCommerce.Catalog',
-                    filename: '[file].map[query]'
+                    namespace: moduleId,
+                    filename: "[file].map[query]"
                 }) :
                 new webpack.SourceMapDevToolPlugin({
-                    namespace: 'VirtoCommerce.Catalog'
+                    namespace: moduleId
                 }) 
                 ,
             new MiniCssExtractPlugin({
-                filename: 'style.css'
+                filename: "style.css"
             })
         ]
     };
