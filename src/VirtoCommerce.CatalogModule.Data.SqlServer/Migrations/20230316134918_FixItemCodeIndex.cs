2b31e652ed999b6f7cab8cea973631f3a9c18d50
Artyom Dudarev <ad@virtoway.com>
2023-03-21 10:23:24 +0200

    PT-11173: Make index IX_Code_CatalogId unique on SQL Server (#667)

---
+++ src/VirtoCommerce.CatalogModule.Data.SqlServer/Migrations/20230316134918_FixItemCodeIndex.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.CatalogModule.Data.SqlServer.Migrations
+{
+    public partial class FixItemCodeIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Code_CatalogId",
+                table: "Item");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Category_Parent_category_check",
+                table: "Category");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Code_CatalogId",
+                table: "Item",
+                columns: new[] { "Code", "CatalogId" },
+                unique: true);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Category_Parent_category_check",
+                table: "Category",
+                sql: "\"ParentCategoryId\" != \"Id\"");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Code_CatalogId",
+                table: "Item");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Category_Parent_category_check",
+                table: "Category");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Code_CatalogId",
+                table: "Item",
+                columns: new[] { "Code", "CatalogId" });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Category_Parent_category_check",
+                table: "Category",
+                sql: "ParentCategoryId != Id");
+        }
+    }
+}
