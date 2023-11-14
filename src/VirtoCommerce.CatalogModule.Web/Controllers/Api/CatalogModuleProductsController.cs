7b3d7780b6a5f7058a45edfc476c9f321cf72aa0
Oleg Zhuk <zhukoo@gmail.com>
2023-11-14 19:14:53 +0200

    PT-14480: Cloned Product doesn't not have the actual creation date (#710)
    fix: Cloned Product doesn't not have the actual creation date.

--- src/VirtoCommerce.CatalogModule.Web/Controllers/Api/CatalogModuleProductsController.cs
+++ src/VirtoCommerce.CatalogModule.Web/Controllers/Api/CatalogModuleProductsController.cs
@@ -224,12 +224,24 @@ namespace VirtoCommerce.CatalogModule.Web.Controllers.Api
 
             var copyProduct = product.GetCopy() as CatalogProduct;
 
+            // Reset 
+            copyProduct.Id = null;
+            copyProduct.CreatedDate = DateTime.UtcNow;
+            copyProduct.CreatedBy = null;
+            copyProduct.ModifiedDate = null;
+            copyProduct.ModifiedBy = null;
+
             // Generate new SKUs and remove SEO records for product and its variations
             copyProduct.Code = _skuGenerator.GenerateSku(product);
             copyProduct.SeoInfos.Clear();
 
             foreach (var variation in copyProduct.Variations)
             {
+                variation.Id = null;
+                variation.CreatedDate = DateTime.UtcNow;
+                variation.CreatedBy = null;
+                variation.ModifiedDate = null;
+                variation.ModifiedBy = null;
                 variation.Code = _skuGenerator.GenerateSku(variation);
                 variation.SeoInfos.Clear();
             }
