1b99b1be50a17b8e81a5d440b7cb56e6f07c5acd
Basil Kotov <basilkot@gmail.com>
2023-12-26 11:36:01 +0000

    check custom property for product

---
+++ src/VirtoCommerce.CatalogModule.Web/Authorization/CustomPropertyRequirementHandler.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using VirtoCommerce.CatalogModule.Core.Model;
+using VirtoCommerce.CatalogModule.Data.Authorization;
+using VirtoCommerce.Platform.Core.Security;
+using VirtoCommerce.Platform.Security.Authorization;
+
+namespace VirtoCommerce.CatalogModule.Web.Authorization
+{
+    public sealed class CustomPropertyRequirementHandler : PermissionAuthorizationHandlerBase<CustomPropertyRequirement>
+    {
+        private readonly MvcNewtonsoftJsonOptions _jsonOptions;
+        public CustomPropertyRequirementHandler(IOptions<MvcNewtonsoftJsonOptions> jsonOptions)
+        {
+            _jsonOptions = jsonOptions.Value;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomPropertyRequirement requirement)
+        {
+            await base.HandleRequirementAsync(context, requirement);
+
+            if (!context.HasSucceeded)
+            {
+                var userPermissions = context.User.FindPermissions(requirement.Permission, _jsonOptions.SerializerSettings);
+
+                if (userPermissions.Count == 0)
+                {
+                    if (context.Resource is IEnumerable<IHasProperties> haveProperties)
+                    {
+                        if (haveProperties.All(x => x.Properties.All(p => p.Id != null)))
+                        {
+                            context.Succeed(requirement);
+                        }
+                    }
+                    else if (context.Resource is IHasProperties hasProperties)
+                    {
+                        if (hasProperties.Properties.All(x => x.Id != null))
+                        {
+                            context.Succeed(requirement);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
