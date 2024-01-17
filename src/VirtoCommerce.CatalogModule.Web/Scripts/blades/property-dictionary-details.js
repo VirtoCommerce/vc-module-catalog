0c4c75f660691ff16d3889ba8c227505f89c26d5
Basil Kotov <basilkot@gmail.com>
2024-01-17 14:29:09 +0000

    change approach to check permission

--- src/VirtoCommerce.CatalogModule.Web/Scripts/blades/property-dictionary-details.js
+++ src/VirtoCommerce.CatalogModule.Web/Scripts/blades/property-dictionary-details.js
@@ -3,12 +3,11 @@ angular.module('virtoCommerce.catalogModule')
         ['$scope', 'platformWebApp.dialogService', 'platformWebApp.bladeNavigationService', 'virtoCommerce.catalogModule.propDictItems', function ($scope, dialogService, bladeNavigationService, propDictItems) {
             var blade = $scope.blade;
             blade.headIcon = 'fa fa-book';
+            blade.updatePermission = 'catalog:dictionary-property:edit';
 
             $scope.isValid = true;
             $scope.blade.isLoading = false;
             $scope.validationRules = blade.property.validationRule;
-
-            var hasEditDictionaryPermission = bladeNavigationService.checkPermission('catalog:dictionary-property:edit');
 
             $scope.setForm = function (form) { blade.formScope = form; }
 
@@ -26,7 +25,7 @@ angular.module('virtoCommerce.catalogModule')
             };
 
             function isDirty() {
-                return !angular.equals(blade.currentEntity, blade.origEntity) && hasEditDictionaryPermission;
+                return !angular.equals(blade.currentEntity, blade.origEntity) && blade.hasUpdatePermission();
             }
 
             function canSave() {
