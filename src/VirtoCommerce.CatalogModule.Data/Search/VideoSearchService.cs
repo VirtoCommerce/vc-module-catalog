d605802c78a75cc13cfb4763981d59419dd9bfd1
Евгений Колосов <kolosov.e.s@gmail.com>
2025-01-31 09:41:22 +0100

    VCST-2600: Clear comment

--- src/VirtoCommerce.CatalogModule.Data/Search/VideoSearchService.cs
+++ src/VirtoCommerce.CatalogModule.Data/Search/VideoSearchService.cs
@@ -42,7 +42,7 @@ namespace VirtoCommerce.CatalogModule.Data.Search
 
             if (!criteria.LanguageCode.IsNullOrEmpty())
             {
-                // Video without language code is considered as a video with any language - VCST-2600
+                // Video without language code is considered as a video with any language
                 query = query.Where(x => x.LanguageCode == criteria.LanguageCode || x.LanguageCode == null);
             }
 
