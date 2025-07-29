bdf9c29988d6ca0e4d7c1af3b3f7a5c6560c37b5
Oleg Zhuk <zhukoo@gmail.com>
2025-07-29 18:34:13 +0200

    VCST-3699: SEO query returns seo information for disabled products and categories
    fix: Resolves the issue when SEO query returns seo information for disabled products and categories instead of 404

--- src/VirtoCommerce.CatalogModule.Data/Services/CatalogSeoResolver.cs
+++ src/VirtoCommerce.CatalogModule.Data/Services/CatalogSeoResolver.cs
@@ -35,7 +35,7 @@ public class CatalogSeoResolver : ISeoResolver
         _storeService = storeService;
     }
 
-    public async Task<IList<SeoInfo>> FindSeoAsync(SeoSearchCriteria criteria)
+    public virtual async Task<IList<SeoInfo>> FindSeoAsync(SeoSearchCriteria criteria)
     {
         ArgumentNullException.ThrowIfNull(criteria);
 
@@ -58,14 +58,6 @@ public class CatalogSeoResolver : ISeoResolver
         if (currentEntitySeoInfos.Count == 0)
         {
             return [];
-
-            // TODO: Uncomment this block of code when the frontend starts to support inactive SEO entries and redirect to the actual ones
-            // Try to find inactive SEO entries
-            //currentEntitySeoInfos = await SearchSeoInfos(segments.Last(), criteria.StoreId, criteria.LanguageCode, isActive: false);
-            //if (currentEntitySeoInfos.Count == 0)
-            //{
-            //    return [];
-            //}
         }
 
         var groups = currentEntitySeoInfos.GroupBy(x => new { x.ObjectType, x.ObjectId }).ToList();
@@ -117,6 +109,42 @@ public class CatalogSeoResolver : ISeoResolver
         return [];
     }
 
+    protected virtual async Task<List<SeoInfo>> SearchSeoInfos(string slug, Store store, string languageCode, bool isActive = true)
+    {
+        using var repository = _repositoryFactory();
+
+        var entities = await repository.SeoInfos
+            .Where(x =>
+                x.IsActive == isActive &&
+                x.Keyword == slug &&
+                (x.Category != null && x.Category.IsActive || x.Item != null && x.Item.IsActive) &&
+                (string.IsNullOrEmpty(x.StoreId) || x.StoreId == store.Id) &&
+                (string.IsNullOrEmpty(x.Language) || x.Language == languageCode || x.Language == store.DefaultLanguage))
+            .ToListAsync();
+
+        return entities
+            .Select(x => x.ToModel(AbstractTypeFactory<SeoInfo>.TryCreateInstance()))
+            .OrderByDescending(GetScore)
+            .ToList();
+
+        int GetScore(SeoInfo seoInfo)
+        {
+            var score = 0;
+            var hasLangCriteria = !string.IsNullOrEmpty(languageCode);
+
+            if (seoInfo.StoreId.EqualsIgnoreCase(store.Id))
+            {
+                score += 2;
+            }
+
+            if (hasLangCriteria && seoInfo.LanguageCode.EqualsIgnoreCase(languageCode))
+            {
+                score += 1;
+            }
+
+            return score;
+        }
+    }
     private async Task<IList<Outline>> GetOutlines(string objectType, string objectId, IList<SeoInfo> infos)
     {
         var outlines = objectType switch
@@ -178,41 +206,5 @@ public class CatalogSeoResolver : ISeoResolver
             .Distinct(StringComparer.OrdinalIgnoreCase);
 
         return immediateParentIds.Any(x => parentIds.Contains(x, StringComparer.OrdinalIgnoreCase));
-    }
-
-    private async Task<List<SeoInfo>> SearchSeoInfos(string slug, Store store, string languageCode, bool isActive = true)
-    {
-        using var repository = _repositoryFactory();
-
-        var entities = await repository.SeoInfos
-            .Where(x =>
-                x.IsActive == isActive &&
-                x.Keyword == slug &&
-                (string.IsNullOrEmpty(x.StoreId) || x.StoreId == store.Id) &&
-                (string.IsNullOrEmpty(x.Language) || x.Language == languageCode || x.Language == store.DefaultLanguage))
-            .ToListAsync();
-
-        return entities
-            .Select(x => x.ToModel(AbstractTypeFactory<SeoInfo>.TryCreateInstance()))
-            .OrderByDescending(GetScore)
-            .ToList();
-
-        int GetScore(SeoInfo seoInfo)
-        {
-            var score = 0;
-            var hasLangCriteria = !string.IsNullOrEmpty(languageCode);
-
-            if (seoInfo.StoreId.EqualsIgnoreCase(store.Id))
-            {
-                score += 2;
-            }
-
-            if (hasLangCriteria && seoInfo.LanguageCode.EqualsIgnoreCase(languageCode))
-            {
-                score += 1;
-            }
-
-            return score;
-        }
     }
 }
