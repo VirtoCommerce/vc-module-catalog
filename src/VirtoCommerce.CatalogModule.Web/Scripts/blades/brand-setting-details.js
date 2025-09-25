7cd88f11ee01584b9341064a2ce61e5cc4f6c476
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-09-25 09:17:17 +0200

    feat: add seo to brands settings blade

--- src/VirtoCommerce.CatalogModule.Web/Scripts/blades/brand-setting-details.js
+++ src/VirtoCommerce.CatalogModule.Web/Scripts/blades/brand-setting-details.js
@@ -1,7 +1,7 @@
 angular.module('virtoCommerce.catalogModule')
     .controller('virtoCommerce.catalogModule.brandSettingDetailsController',
-        ['$scope', '$q', '$timeout', 'virtoCommerce.catalogModule.brandSettings', 'virtoCommerce.catalogModule.catalogs', 'virtoCommerce.catalogModule.aggregationProperties',
-            function ($scope, $q, $timeout, brandSettings, catalogs, aggregationProperties) {
+        ['$scope', '$q', '$timeout', 'platformWebApp.bladeNavigationService', 'virtoCommerce.catalogModule.brandSettings', 'virtoCommerce.catalogModule.catalogs', 'virtoCommerce.catalogModule.aggregationProperties',
+            function ($scope, $q, $timeout, bladeNavigationService, brandSettings, catalogs, aggregationProperties) {
                 const blade = $scope.blade;
                 blade.title = 'catalog.blades.brand-setting-details.title';
                 blade.updatePermission = 'brands:update';
@@ -13,10 +13,10 @@ angular.module('virtoCommerce.catalogModule')
 
                     if (blade.store) {
                         var getStorePromise = brandSettings.getByStore({ storeId: blade.store.id }).$promise.then(function (data) {
-                            return data
+                            return data;
                         });
                         var getPropertiesPromise = aggregationProperties.getProperties({ storeId: blade.store.id }).$promise.then(function (data) {
-                            return data
+                            return data;
                         });
 
                         $q.all([
@@ -24,30 +24,61 @@ angular.module('virtoCommerce.catalogModule')
                             getPropertiesPromise,
                         ]).then(function (results) {
                             if (results) {
-                                initialize(results[0]);
-
-                                blade.properties = results[1];
-
-                                $timeout(resetBrandSettings, 0);
-                                blade.isLoading = false;
+                                var data = results[0];
+                                var properties = results[1];
+                                if (data.brandCatalogId) {
+                                    blade.catalog = catalogs.get({ id: data.brandCatalogId }, function (catalog) {
+                                        //data.selectedCatalog = catalog;
+                                        initialize(data, properties, catalog);
+                                    });
+                                }
+                                else {
+                                    initialize(data, properties);
+                                }
                             }
                         });
                     }
                 };
 
-                function initialize(data) {
+                function initialize(data, properties, catalog) {
                     blade.currentEntity = angular.copy(data);
                     blade.originalEntity = data;
+
+                    blade.properties = properties;
+
+                    if (catalog) {
+                        blade.selectedCatalog = angular.copy(catalog);
+                        blade.originalCatalog = catalog;
+                    }
+
+                    $timeout(resetBrandSettings, 0);
+                    blade.isLoading = false;
                 }
 
                 blade.saveChanges = function () {
                     blade.isLoading = true;
 
-                    brandSettings.updateSetting(blade.currentEntity,
-                        function () {
-                            blade.isNew = false;
-                            blade.refresh();
+                    let saveTasks = [];
+
+                    if (selectedCatalogSeoIsDirty()) {
+                        var updateCatalogPromise = catalogs.update(blade.selectedCatalog).$promise.then(function (data) {
+                            return data;
                         });
+
+                        saveTasks.push(updateCatalogPromise);
+                    }
+
+                    if (currentEntityIsDirty()) {
+                        var updateSettingsPromise = brandSettings.updateSetting(blade.currentEntity).$promise.then(function () {
+                            return undefined;
+                        });
+                        saveTasks.push(updateSettingsPromise);
+                    }
+
+                    $q.all(saveTasks).then(function () {
+                        closeChildrenBlades();
+                        blade.refresh();
+                    });
                 };
 
                 $scope.setForm = function (form) {
@@ -59,7 +90,15 @@ angular.module('virtoCommerce.catalogModule')
                 }
 
                 function isDirty() {
-                    return !angular.equals(blade.currentEntity, blade.originalEntity) && blade.hasUpdatePermission();
+                    return (currentEntityIsDirty() || selectedCatalogSeoIsDirty()) && blade.hasUpdatePermission();
+                }
+
+                function currentEntityIsDirty() {
+                    return !angular.equals(blade.currentEntity, blade.originalEntity);
+                }
+
+                function selectedCatalogSeoIsDirty() {
+                    return blade.selectedCatalog && blade.originalCatalog && !angular.equals(blade.selectedCatalog.seoInfos, blade.originalCatalog.seoInfos);
                 }
 
                 blade.fetchCatalogs = function (criteria) {
@@ -76,9 +115,26 @@ angular.module('virtoCommerce.catalogModule')
                     });
                 }
 
+                blade.onSelectCatalog = function (item) {
+                    closeChildrenBlades();
+                    blade.selectedCatalog = item;
+                }
+
+                blade.onRemoveCatalog = function () {
+                    closeChildrenBlades();
+                    blade.selectedCatalog = undefined;
+                }
+
                 function resetBrandSettings() {
                     $scope.brandSettingsLoaded = true;
                 }
+
+                function closeChildrenBlades() {
+                    angular.forEach(blade.childrenBlades.slice(), function (child) {
+                        bladeNavigationService.closeBlade(child);
+                    });
+                }
+
 
                 blade.toolbarCommands = [
                     {
