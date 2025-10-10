bdf6d4ee03e4e6e212536ee5a8d5b7b298a252e9
Oleg Zhuk <zhukoo@gmail.com>
2025-10-10 15:44:28 +0200

    VCST-4076: Product links do not work when using Select All with a keyword filter (#839)

--- src/VirtoCommerce.CatalogModule.Core/Search/SearchServiceExtensions.cs
+++ src/VirtoCommerce.CatalogModule.Core/Search/SearchServiceExtensions.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using VirtoCommerce.CatalogModule.Core.Model;
+using VirtoCommerce.CatalogModule.Core.Model.ListEntry;
 using VirtoCommerce.CatalogModule.Core.Model.Search;
 using VirtoCommerce.Platform.Core.Common;
 
@@ -8,6 +9,85 @@ namespace VirtoCommerce.CatalogModule.Core.Search;
 
 public static class SearchServiceExtensions
 {
+    public static async Task<IList<ListEntryBase>> SearchAllAsync(this IInternalListEntrySearchService searchService, CatalogListEntrySearchCriteria searchCriteria, ICancellationToken cancellationToken)
+    {
+        var result = new List<ListEntryBase>();
+
+        await foreach (var searchResult in searchService.SearchBatchesAsync(searchCriteria, cancellationToken))
+        {
+            result.AddRange(searchResult.Results);
+        }
+
+        return result;
+    }
+
+    public static async IAsyncEnumerable<ListEntrySearchResult> SearchBatchesAsync(this IInternalListEntrySearchService searchService, CatalogListEntrySearchCriteria searchCriteria, ICancellationToken cancellationToken)
+    {
+        int totalCount;
+        searchCriteria = searchCriteria.CloneTyped();
+
+        do
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var searchResult = await searchService.InnerListEntrySearchAsync(searchCriteria);
+
+            if (searchCriteria.Take == 0 ||
+                searchResult.Results.Count > 0)
+            {
+                yield return searchResult;
+            }
+
+            if (searchCriteria.Take == 0)
+            {
+                yield break;
+            }
+
+            totalCount = searchResult.TotalCount;
+            searchCriteria.Skip += searchCriteria.Take;
+        }
+        while (searchCriteria.Skip < totalCount);
+    }
+
+    public static async Task<IList<ListEntryBase>> SearchAllAsync(this IListEntrySearchService searchService, CatalogListEntrySearchCriteria searchCriteria, ICancellationToken cancellationToken)
+    {
+        var result = new List<ListEntryBase>();
+
+        await foreach (var searchResult in searchService.SearchBatchesAsync(searchCriteria, cancellationToken))
+        {
+            result.AddRange(searchResult.Results);
+        }
+
+        return result;
+    }
+
+    public static async IAsyncEnumerable<ListEntrySearchResult> SearchBatchesAsync(this IListEntrySearchService searchService, CatalogListEntrySearchCriteria searchCriteria, ICancellationToken cancellationToken)
+    {
+        int totalCount;
+        searchCriteria = searchCriteria.CloneTyped();
+
+        do
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var searchResult = await searchService.SearchAsync(searchCriteria);
+
+            if (searchCriteria.Take == 0 ||
+                searchResult.Results.Count > 0)
+            {
+                yield return searchResult;
+            }
+
+            if (searchCriteria.Take == 0)
+            {
+                yield break;
+            }
+
+            totalCount = searchResult.TotalCount;
+            searchCriteria.Skip += searchCriteria.Take;
+        }
+        while (searchCriteria.Skip < totalCount);
+    }
+
+
     public static async Task<IList<CategoryLink>> SearchAllAsync(this ILinkSearchService searchService, LinkSearchCriteria searchCriteria, ICancellationToken cancellationToken)
     {
         var result = new List<CategoryLink>();
