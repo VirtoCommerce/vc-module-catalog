99840db018833b8adbe9639fd167fb9337a6de1a
Artem Dudarev <artem@virtoworks.com>
2025-12-11 11:00:31 +0200

    VCST-4317: Add DisplayOrder to PropertyValue (#846)

---
+++ src/VirtoCommerce.CatalogModule.Data.MySql/Migrations/20251203133540_Catalog_AddPropertyValueDisplayOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.CatalogModule.Data.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Catalog_AddPropertyValueDisplayOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "PropertyValue",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "PropertyValue");
+        }
+    }
+}
