f6fb7bcda9f1b58b814b4cd59d8d73ba45906409
Artem Dudarev <artem@virtoworks.com>
2025-12-19 12:13:24 +0200

    Fix tests

--- tests/VirtoCommerce.CatalogModule.Tests/AggregationRequestBuilderTests.cs
+++ tests/VirtoCommerce.CatalogModule.Tests/AggregationRequestBuilderTests.cs
@@ -11,14 +11,15 @@ namespace VirtoCommerce.CatalogModule.Tests
     public class AggregationRequestBuilderTests : BrowseFiltersTestBase
     {
         [Theory]
-        [InlineData("", null, null, "10")]
-        [InlineData("Unknown:10", "Unknown:10", "Unknown:10", "10")]
-        [InlineData("Unknown:10;Color:Red", "(Unknown:10 AND Color:Red)", "Unknown:10", "10")]
-        [InlineData("Unknown:10;Color:Red;Size:size1", "(Unknown:10 AND Color:Red AND Size:[TO 10))", "(Unknown:10 AND Size:[TO 10))", "10")]
-        [InlineData("Color:Pink", "Color:Pink", null, "10")]
-        [InlineData("Size:unknown", "ID:", "ID:", "10")]
-        public async Task TestSimpleAggregations(string terms, string expectedFilter1, string expectedFilter2, string expectedRange)
+        [InlineData("", null, null, null)]
+        [InlineData("Unknown:10", "Unknown:10", "Unknown:10", "Unknown:10")]
+        [InlineData("Unknown:10;Color:Red", "(Unknown:10 AND Color:Red)", "Unknown:10", "(Unknown:10 AND Color:Red)")]
+        [InlineData("Unknown:10;Color:Red;Size:size1", "(Unknown:10 AND Color:Red AND Size:[TO 10))", "(Unknown:10 AND Size:[TO 10))", "(Unknown:10 AND Color:Red)")]
+        [InlineData("Color:Pink", "Color:Pink", null, "Color:Pink")]
+        [InlineData("Size:unknown", "ID:", "ID:", null)]
+        public async Task TestSimpleAggregations(string terms, string expectedFilter1, string expectedFilter2, string expectedFilter3)
         {
+            // Arrange
             var criteria = new ProductIndexedSearchCriteria
             {
                 Terms = terms?.Split(';'),
@@ -26,8 +27,10 @@ namespace VirtoCommerce.CatalogModule.Tests
 
             var allFilters = await GetTermFilterBuilder().GetTermFiltersAsync(criteria);
 
+            // Act
             var requests = await GetAggregationRequestBuilder().GetAggregationRequestsAsync(criteria, allFilters);
 
+            // Assert
             Assert.Equal(3, requests.Count);
 
             Assert.IsType<TermAggregationRequest>(requests[0]);
@@ -49,16 +52,23 @@ namespace VirtoCommerce.CatalogModule.Tests
             Assert.IsType<RangeAggregationRequest>(requests[2]);
             var rangeRequest = (RangeAggregationRequest)requests[2];
             Assert.Equal("Size", rangeRequest.FieldName);
+            Assert.Equal(expectedFilter3, rangeRequest.Filter?.ToString());
+            Assert.Equal("Size-size1-size2", rangeRequest.Id);
+            Assert.Equal(2, rangeRequest.Values.Count);
 
             var rangeRequestValue = rangeRequest.Values[0];
             Assert.Equal("size1", rangeRequestValue.Id);
-            Assert.Equal(expectedRange, rangeRequestValue.Upper);
             Assert.Null(rangeRequestValue.Lower);
+            Assert.True(rangeRequestValue.IncludeLower);
+            Assert.Equal("10", rangeRequestValue.Upper);
+            Assert.False(rangeRequestValue.IncludeUpper);
 
             rangeRequestValue = rangeRequest.Values[1];
             Assert.Equal("size2", rangeRequestValue.Id);
+            Assert.Equal("10", rangeRequestValue.Lower);
+            Assert.True(rangeRequestValue.IncludeLower);
             Assert.Null(rangeRequestValue.Upper);
-            Assert.Equal(expectedRange, rangeRequestValue.Lower);
+            Assert.False(rangeRequestValue.IncludeUpper);
         }
 
         private static IAggregationConverter GetAggregationRequestBuilder()
